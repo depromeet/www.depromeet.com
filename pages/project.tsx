3a3b25287fdaf21341ac469e3f7ae5c6dce730f2
wurikiji <wurikiji@gmail.com>
2021-02-24 03:37:57 +0900

    fix: add horizontal scroll in project page

--- pages/project.tsx
+++ pages/project.tsx
@@ -11,7 +11,7 @@ import projectsData from '../resources/data/projects';
 const Project = () => {
   const BackgroundImage = dynamic(() => import('../resources/images/project_background_image.svg'));
   return (
-    <>
+    <div className="no-scroll-bar" style={{ overflow: 'scroll' }}>
       <Header />
       <TopBackground>
         <div style={{ width: '192rem' }}>
@@ -27,7 +27,7 @@ const Project = () => {
         <AllSchedule />
       </CenterAlignedContainer>
       <Footer />
-    </>
+    </div>
   );
 };
 
