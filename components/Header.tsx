5c574b4722fdc77e6572fdc3f009faf056c4195f
wurikiji <wurikiji@gmail.com>
2021-02-25 14:16:47 +0900

    feature: font preloading

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { FC, MutableRefObject, useState } from 'react';
+import React, { FC, useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import dynamic from 'next/dynamic';
