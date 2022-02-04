8547e0a6b999ffd13682f247d4ca3026669b54c3
Jongho Park <jong951005@gmail.com>
2022-02-05 03:28:58 +0900

    Section1 애니메이션 추가

--- containers/Index/Section1.tsx
+++ containers/Index/Section1.tsx
@@ -66,19 +66,35 @@ const Subtitle = styled.p`
 `;
 
 const RotateBackground = styled.div`
+  @property --a {
+    syntax: '<angle>';
+    inherits: false;
+    initial-value: -270deg;
+  }
+
   z-index: -1;
   position: absolute;
   width: 100%;
   height: 100vh;
 
-  transform: rotate(-180deg);
+  --a: 270deg;
   background: conic-gradient(
-    from 270deg at 50% 50%,
+    from var(--a) at 50% 50%,
     rgba(49, 107, 255, 0) 0deg,
     rgba(49, 107, 255, 0.6) 179.07deg,
     rgba(49, 107, 255, 0.8) 269.28deg,
     #316bff 360deg
   );
+
+  animation: 25s rotate linear infinite;
+  @keyframes rotate {
+    from {
+      --a: -270deg;
+    }
+    to {
+      --a: 90deg;
+    }
+  }
 `;
 
 const Highlight = styled.span`
