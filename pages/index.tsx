7b01d8b5d68096628a1070763ab68df55e2fa126
Jongho Park <jong951005@gmail.com>
2022-02-05 02:43:17 +0900

    IndexPage 쓸모 없는 로직 제거

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import Image from 'next/image';
 
 import { Header, Footer } from '../components';
 
@@ -8,8 +7,6 @@ import Section2 from '../containers/Index/Section2';
 import Section3 from '../containers/Index/Section3';
 import Section4 from '../containers/Index/Section4';
 import { DeviceContextProvider } from '../contexts/device';
-
-import { media } from '../styles/theme';
 
 const Index = () => (
   <DeviceContextProvider>
@@ -20,58 +17,10 @@ const Index = () => (
         <Section2 />
         <Section3 />
         <Section4 />
-        {/* <Background>
-        <BackgroundImage />
-        <BackgroundImage forMobile />
-      </Background> */}
       </Containers>
       <Footer />
     </div>
   </DeviceContextProvider>
-);
-
-const Background = styled.div`
-  z-index: -1;
-
-  position: absolute;
-  bottom: 54rem;
-  left: 0;
-  right: 0;
-  height: 32rem;
-  display: flex;
-  justify-content: center;
-
-  ${media.mobile} {
-    height: 90vh;
-    bottom: 0;
-  }
-
-  .bg-image {
-    position: relative;
-    width: 290rem;
-    margin-left: 8rem;
-    flex-shrink: 0;
-    :first-child {
-      margin-left: 0;
-    }
-
-    &.mobile__only {
-      margin-left: 0;
-    }
-  }
-`;
-
-const BackgroundImage = ({ forMobile = false }) => (
-  <div className={`bg-image ${forMobile ? 'mobile__only' : 'mobile__none'}`}>
-    <Image
-      className="bg-image--image"
-      src={'/bottom-background.svg'}
-      alt="11th background"
-      layout="fill"
-      objectFit="contain"
-      loading="lazy"
-    />
-  </div>
 );
 
 const Containers = styled.div`
