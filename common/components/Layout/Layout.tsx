ffbf35594b4a0d4f7a78f587225c39c527d992af
jonghopark95 <jong951005@gmail.com>
2022-06-08 23:01:03 +0900

    Layout 컴포넌트 구현

---
+++ common/components/Layout/Layout.tsx
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled';
+
+import { Device, DeviceContextProvider, useDeviceContext } from 'contexts/device';
+
+type Props = {
+  children: (device: Device) => React.ReactNode;
+  header?: React.ReactNode;
+  footer?: React.ReactNode;
+};
+
+function Content({ children }: Props) {
+  const device = useDeviceContext();
+
+  return <>{children(device)}</>;
+}
+
+export default function Layout({ children, header, footer }: Props) {
+  return (
+    <DeviceContextProvider>
+      <Container>
+        {header}
+
+        <Content children={children} />
+
+        {footer}
+      </Container>
+    </DeviceContextProvider>
+  );
+}
+
+const Container = styled.div`
+  box-sizing: border-box;
+  position: relative;
+
+  width: 100%;
+  height: 100vh;
+
+  overflow: scroll;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
--- common/components/index.ts
+++ common/components/index.ts
@@ -1,1 +1,3 @@
 export { default as Header } from './Header';
+export { default as Layout } from './Layout';
+export { default as Footer } from './Footer';
