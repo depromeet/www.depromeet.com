2d8e01e4948fc50894800ab41af20f8939df1fcc
Hyesung Oh <haesungoh414@gmail.com>
2022-08-14 21:42:43 +0900

    env: setting gzip compress, bundle analyzer and baseUrl

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
+const withPlugins = require('next-compose-plugins');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+const CompressionPlugin = require('compression-webpack-plugin');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: config => {
+    config.plugins.push(new CompressionPlugin());
+    return config;
+  },
 };
 
-module.exports = nextConfig;
+module.exports = withPlugins([withBundleAnalyzer], nextConfig);
