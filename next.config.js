e7049d8a15ebdc372f78f6a9f1bf1db7931dac64
정대윤 <sensecodevalue@gmail.com>
2022-08-17 03:22:49 +0900

    fix: Invalid next.config.js options detected

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-const withPlugins = require('next-compose-plugins');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
@@ -14,4 +13,12 @@ const nextConfig = {
   },
 };
 
-module.exports = withPlugins([withBundleAnalyzer], nextConfig);
+/**
+ *
+ * @link https://github.com/cyrilwanner/next-compose-plugins/issues/59#issuecomment-1209152211
+ * warnning 해결 입니다. "next-compose-plugins"의 이슈로 defaultConfig를 모두 정의해줘야되는 wanning이 떠서 시원하게 없앴습니다.
+ */
+module.exports = () => {
+  const plugins = [withBundleAnalyzer];
+  return plugins.reduce((acc, plugin) => plugin(acc), { ...nextConfig });
+};
