f3d3457fd780ed819ac238434c9b26f845091ad8
hyesung oh <haesungoh414@gmail.com>
2022-08-17 10:45:20 +0900

    Footer 개발 및 적용 (#90)
    * feat: Social media 아이콘 추가
    
    * feat: 디프만 링크 상수 설정
    
    * design: header 섹션 scroll bottom 애니메이션 적용
    
    * feat: Footer 개발 및 적용

---
+++ src/components/common/icons/MailIcon.tsx
@@ -0,0 +1,12 @@
+import Svg from './Svg';
+
+export function MailIcon() {
+  return (
+    <Svg width="24" height="24" viewBox="0 0 24 24">
+      <path
+        fill="white"
+        d="M0 3v18h24v-18h-24zm21.518 2l-9.518 7.713-9.518-7.713h19.036zm-19.518 14v-11.817l10 8.104 10-8.104v11.817h-20z"
+      />
+    </Svg>
+  );
+}
--- src/components/common/icons/index.tsx
+++ src/components/common/icons/index.tsx
@@ -1,1 +1,7 @@
+export { BehanceIcon } from './BehanceIcon';
+export { FacebookIcon } from './FacebookIcon';
+export { GithubIcon } from './GithubIcon';
+export { InstagramIcon } from './InstagramIcon';
+export { MailIcon } from './MailIcon';
+export { MediumIcon } from './MediumIcon';
 export { ScrollBottomIcon } from './ScrollBottomIcon';
