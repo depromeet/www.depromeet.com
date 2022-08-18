9870da394b1b892bd54babc72f98514472cf18f5
hyesung oh <haesungoh414@gmail.com>
2022-08-19 00:28:45 +0900

    NavigationBar 적용 (#95)
    * feat: NavigationBar 개발 및 적용
    
    * feat: NavigationBar 고정
    
    * design: Navigation anchor 애니메이션 적용

---
+++ src/components/common/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,96 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { css } from '@emotion/react';
+
+import { colors } from '~/styles/constants';
+import { layoutCss } from '~/styles/css';
+
+interface Route {
+  name: string;
+  path: string;
+}
+
+const ROUTES: Route[] = [
+  { name: '프로젝트', path: '/project' },
+  { name: '문의하기', path: '/contact' },
+  { name: '리쿠르트', path: '/recruit' },
+];
+
+export default function NavigationBar() {
+  const router = useRouter();
+
+  console.log(router.asPath);
+
+  return (
+    <nav css={navCss}>
+      <div css={wrapperCss}>
+        <div>
+          <Link href="/">디프만 로고</Link>
+        </div>
+
+        <div css={anchorWrapperCss}>
+          {ROUTES.map(route => (
+            <Link key={route.name} href={route.path}>
+              <a css={anchorCss(router.asPath, router.asPath.includes(route.path))}>{route.name}</a>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+const navCss = css`
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 60px;
+  background-color: ${colors.black};
+  z-index: 9999;
+`;
+
+const wrapperCss = css`
+  ${layoutCss}
+  height: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const anchorWrapperCss = css`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  gap: 2.5rem;
+`;
+
+const anchorCss = (path: string, isActive: boolean) => css`
+  font-size: 1rem;
+  line-height: 118.75%;
+
+  padding: 10px 20px;
+  border-radius: 8px;
+  transition: background-color 0.3s, color 0.3s;
+
+  &:hover {
+    background-color: ${colors.gray9};
+  }
+
+  ${isActive ? activeAnchorCss : inactiveAnchorCss}
+  ${(path === '/' || path === '/interview') && defaultAnchorCss}
+`;
+
+const defaultAnchorCss = css`
+  color: ${colors.white};
+`;
+
+const activeAnchorCss = css`
+  font-weight: 700;
+`;
+
+const inactiveAnchorCss = css`
+  color: ${colors.gray2};
+`;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app';
+import { css } from '@emotion/react';
 
 import Footer from '~/components/common/Footer';
+import NavigationBar from '~/components/common/NavigationBar';
 import { layoutCss } from '~/styles/css';
 import GlobalStyle from '~/styles/GlobalStyle';
 
@@ -8,10 +10,17 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
-      <div css={layoutCss}>
+
+      <NavigationBar />
+      <div css={contentLayoutCss}>
         <Component {...pageProps} />
       </div>
       <Footer />
     </>
   );
 }
+
+const contentLayoutCss = css`
+  ${layoutCss}
+  padding-top: 60px; // NOTE: NavigationBar 높이
+`;
