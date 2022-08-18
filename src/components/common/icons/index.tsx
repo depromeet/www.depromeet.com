fcecad8d5f22d739e6aaa486c19d8fbd4bbab4b1
Hyesung Oh <haesungoh414@gmail.com>
2022-08-19 02:17:21 +0900

    feat: Icon Props 추가

--- src/components/common/icons/index.tsx
+++ src/components/common/icons/index.tsx
@@ -2,6 +2,7 @@ export { BehanceIcon } from './BehanceIcon';
 export { FacebookIcon } from './FacebookIcon';
 export { GithubIcon } from './GithubIcon';
 export { InstagramIcon } from './InstagramIcon';
+export { LinkedinIcon } from './LinkedinIcon';
 export { MailIcon } from './MailIcon';
 export { MediumIcon } from './MediumIcon';
 export { ScrollBottomIcon } from './ScrollBottomIcon';
