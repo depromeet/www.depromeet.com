b80f4b19dc257e6b41d6cf427ada35da6a90eb85
Hyesung Oh <haesungoh414@gmail.com>
2022-08-19 00:17:45 +0900

    feat: NavigationBar 개발 및 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 
 import Footer from '~/components/common/Footer';
+import NavigationBar from '~/components/common/NavigationBar';
 import { layoutCss } from '~/styles/css';
 import GlobalStyle from '~/styles/GlobalStyle';
 
@@ -8,6 +9,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
+
+      <NavigationBar />
       <div css={layoutCss}>
         <Component {...pageProps} />
       </div>
