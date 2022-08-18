80382654dd84c23d70eb1bc14cac0fd41404e3be
Hyesung Oh <haesungoh414@gmail.com>
2022-08-19 01:01:49 +0900

    chore: contact 이미지 업로드 및 상수 생성

---
+++ src/constants/images/images.ts
@@ -0,0 +1,7 @@
+const CONTACT_IMAGE_BASE = '/images/contact';
+
+export const CONTACT_IMAGES = {
+  gmail: `${CONTACT_IMAGE_BASE}/gmail.png`,
+  instagram: `${CONTACT_IMAGE_BASE}/instagram.png`,
+  kakaotalk: `${CONTACT_IMAGE_BASE}/kakaotalk.png`,
+} as const;
---
+++ src/constants/images/index.ts
@@ -0,0 +1,1 @@
+export { CONTACT_IMAGES } from './images';
