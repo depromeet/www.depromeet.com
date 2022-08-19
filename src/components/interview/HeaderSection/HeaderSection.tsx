b3824101439d99a2ff5d5d12d3de198824e1f461
Hyesung Oh <haesungoh414@gmail.com>
2022-08-20 01:18:18 +0900

    design: interview header section 모바일 대응

--- src/components/interview/HeaderSection/HeaderSection.tsx
+++ src/components/interview/HeaderSection/HeaderSection.tsx
@@ -1,17 +1,19 @@
 import { css } from '@emotion/react';
 
+import { mediaQuery } from '~/styles/constants';
+
 export default function HeaderSection() {
-  return (
-    <header
-      css={css`
-        position: relative;
-        width: 100vw;
-        left: calc(-50vw + 50%);
-        height: 400px;
-        background-color: gray;
-      `}
-    >
-      아마도 그래픽
-    </header>
-  );
+  return <header css={headerCss}>아마도 그래픽</header>;
 }
+
+const headerCss = css`
+  position: relative;
+  width: 100vw;
+  left: calc(-50vw + 50%);
+  height: 400px;
+  background-color: gray;
+
+  ${mediaQuery('xs')} {
+    height: 200px;
+  }
+`;
