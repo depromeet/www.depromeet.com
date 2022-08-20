dff28489efd102456fb0647b92f5504ac7497a06
SeongYongLee <seongyong.dev@gmail.com>
2022-08-20 17:47:37 +0900

    change HeaderSection tag

--- src/components/project/DescriptionSection/DescriptionSection.tsx
+++ src/components/project/DescriptionSection/DescriptionSection.tsx
@@ -4,14 +4,17 @@ import { colors } from '~/styles/constants';
 
 export default function HeaderSection() {
   return (
-    <div css={descriptionCss}>
-      <div>디프만에서 탄생한 개성만점</div>
-      <div>서비스들을 소개할게요</div>
-    </div>
+    <header css={headerCss}>
+      <h2>
+        디프만에서 탄생한 개성만점
+        <br />
+        서비스들을 소개할게요
+      </h2>
+    </header>
   );
 }
 
-const descriptionCss = css`
+const headerCss = css`
   font-weight: 600;
   font-size: 2rem;
   line-height: 150%;
