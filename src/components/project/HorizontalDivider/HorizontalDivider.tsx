4662b451afb2c20ff3f8c0ab807580e7648595ad
SeongYongLee <seongyong.dev@gmail.com>
2022-08-20 17:42:54 +0900

    change HorizontalDivider tag

--- src/components/project/HorizontalDivider/HorizontalDivider.tsx
+++ src/components/project/HorizontalDivider/HorizontalDivider.tsx
@@ -3,11 +3,9 @@ import { css } from '@emotion/react';
 import { colors } from '~/styles/constants';
 
 export default function HorizontalDivider() {
-  return <div css={horizontalDividerCss} />;
+  return <hr css={horizontalDividerCss} />;
 }
 
 const horizontalDividerCss = css`
-  background-color: ${colors.gray7};
-  width: 100%;
-  height: 1px;
+  border: thin solid ${colors.gray7};
 `;
