924455eecffa8e4f197947410237969be960771f
정대윤 <sensecodevalue@gmail.com>
2022-08-21 23:28:26 +0900

    fix: 13 to 14

--- src/constants/common/common.ts
+++ src/constants/common/common.ts
@@ -9,7 +9,7 @@ export const NOTION_RECRUIT_PATH =
   'https://www.notion.so/depromeet/12-8e4fc4ef30fd4853a522258d117d3c33';
 
 export const START_DATE = '2022-08-21T15:00:00.000Z';
-export const END_DATE = '2022-09-02T13:59:59.000Z';
+export const END_DATE = '2022-09-02T14:59:59.000Z';
 
 // export const START_DATE = '2022-08-18T22:21:59.000Z'; // test
 // export const END_DATE = '2022-08-21T20:00:00.000Z'; // test
