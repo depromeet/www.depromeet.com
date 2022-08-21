ebeebcc5920c54537f1be1254ee7b4c6bf1413f4
Hyesung Oh <haesungoh414@gmail.com>
2022-08-21 15:16:56 +0900

    fix: typo at contact ContactSection

--- src/components/home/RecordSection/RecordSection.tsx
+++ src/components/home/RecordSection/RecordSection.tsx
@@ -21,7 +21,8 @@ export default function RecordSection() {
       viewport={{ amount: 0.6, once: true }}
     >
       <motion.h2 css={headingCss} variants={defaultFadeInVariants}>
-        디프만은 서비스 기획부터 론칭, 그리고 개선까지<br></br> 경험하며 성장하는 모임이에요.
+        디프만은 서비스 기획부터 론칭, 그리고 개선까지
+        <br /> 경험하며 성장하는 모임이에요.
       </motion.h2>
 
       <motion.div css={cardWrapperCss} variants={staggerOne}>
