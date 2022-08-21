e25180011d400b4bd76c2842ff21ac1d0f2042c2
Hyesung Oh <haesungoh414@gmail.com>
2022-08-22 00:14:50 +0900

    feat: Main heading 적용

--- src/components/home/HeaderSection/HeaderSection.tsx
+++ src/components/home/HeaderSection/HeaderSection.tsx
@@ -3,9 +3,11 @@ import { css } from '@emotion/react';
 import { motion, useScroll, useTransform, Variants } from 'framer-motion';
 
 import { ScrollBottomIcon } from '~/components/common/icons';
+import { DepromeetIcon } from '~/components/common/icons/DepromeetIcon';
 import { NAV_HEIGHT } from '~/components/common/NavigationBar/NavigationBar';
 import { defaultEasing } from '~/constants/motions';
 import useMediaQuery from '~/hooks/use-media-query';
+import { mediaQuery } from '~/styles/constants';
 
 const HOME_HEADER_BASE = '/images/home';
 const HEADER_IMAGE = `${HOME_HEADER_BASE}/home_header.png`;
@@ -31,6 +33,14 @@ export default function HeaderSection() {
         objectFit="cover"
       />
 
+      <div css={headingWrapperCss}>
+        <div css={logoWrapperCss}>
+          <DepromeetIcon />
+        </div>
+
+        <h1 css={heading1Css}>디자이너와 {isMobile && <br />}프로그래머가 만났을 때</h1>
+      </div>
+
       <motion.div
         css={scrollBottomIconWrapperCss}
         style={{ opacity: scrollBottomOpacity, scale: scrollBottomScale }}
@@ -51,6 +61,43 @@ const headerCss = css`
   left: calc(-50vw + 50%);
 
   height: calc(100vh - ${NAV_HEIGHT}px);
+`;
+
+const headingWrapperCss = css`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`;
+
+const logoWrapperCss = css`
+  max-width: 490px;
+  height: 90px;
+
+  margin-bottom: 19px;
+
+  ${mediaQuery('xs')} {
+    max-width: 277px;
+    height: 51px;
+
+    margin-bottom: 15px;
+  }
+`;
+
+const heading1Css = css`
+  font-weight: 500;
+  font-size: 3.125rem;
+  line-height: 150%;
+  white-space: pre;
+
+  ${mediaQuery('xs')} {
+    font-size: 24px;
+  }
 `;
 
 const scrollBottomIconWrapperCss = css`
