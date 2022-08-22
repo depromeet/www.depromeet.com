ba8bed2c932ab667400ce4dc14be33840978d7bd
Hyesung Oh <haesungoh414@gmail.com>
2022-08-22 17:45:18 +0900

    feat: project SEO 적용

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,13 +1,17 @@
+import SEO from '~/components/common/SEO';
 import DescriptionSection from '~/components/project/DescriptionSection';
 import HorizontalDivider from '~/components/project/HorizontalDivider';
 import ProjectSection from '~/components/project/ProjectSection';
 
 export default function Project() {
   return (
-    <main>
-      <DescriptionSection />
-      <HorizontalDivider />
-      <ProjectSection />
-    </main>
+    <>
+      <SEO title="디프만 - Project" />
+      <main>
+        <DescriptionSection />
+        <HorizontalDivider />
+        <ProjectSection />
+      </main>
+    </>
   );
 }
