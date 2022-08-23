34d5cf6407e8cf6fdbd2df070da244d051bb90ba
hyesung oh <haesungoh414@gmail.com>
2022-08-24 00:05:27 +0900

    recruit/[position] route SSG 적용 (#151)
    refactor: position route SSG 적용

--- src/pages/recruit/[position]/index.tsx
+++ src/pages/recruit/[position]/index.tsx
@@ -1,28 +1,52 @@
-import Error from 'next/error';
-import { useRouter } from 'next/router';
-import { ParsedUrlQuery } from 'querystring';
-
 import SEO from '~/components/common/SEO';
 import { POSITION_TYPE, PositionType } from '~/components/recruit-detail/constants';
 import DescriptionSection from '~/components/recruit-detail/DescriptionSection';
 import HeaderSection from '~/components/recruit-detail/HeaderSection';
 import OtherPositionSection from '~/components/recruit-detail/OtherPositionSection';
 
-export default function RecruitDetail() {
-  const router = useRouter();
-  const { position } = router.query as ParsedUrlQuery & { position: string };
-  const positionType = POSITION_TYPE[position?.toUpperCase() as PositionType];
+interface Props {
+  position: typeof POSITION_TYPE[PositionType];
+}
 
-  if (!position && !positionType) return <Error statusCode={404} />;
-
+export default function RecruitDetail({ position }: Props) {
   return (
     <>
       <SEO title={`디프만 - ${position}`} />
       <main>
-        <HeaderSection positionType={positionType} />
-        <DescriptionSection positionType={positionType} />
-        <OtherPositionSection positionType={positionType} />
+        <HeaderSection positionType={position} />
+        <DescriptionSection positionType={position} />
+        <OtherPositionSection positionType={position} />
       </main>
     </>
   );
+}
+
+interface Paths {
+  params: {
+    // NOTE: 대소문자 구분을 위해 string으로 정의
+    position: string;
+  };
+}
+
+export async function getStaticPaths() {
+  const paths: Paths[] = [];
+
+  Object.values(POSITION_TYPE).forEach(position => {
+    paths.push({ params: { position: position.toLowerCase() } });
+  });
+
+  return { paths, fallback: 'blocking' };
+}
+
+export async function getStaticProps({ params }: Paths) {
+  const { position } = params;
+  const upperCasePosition = position.toUpperCase();
+
+  if (!POSITION_TYPE.hasOwnProperty(upperCasePosition)) {
+    return { notFound: true };
+  }
+
+  return {
+    props: { position: upperCasePosition },
+  };
 }
