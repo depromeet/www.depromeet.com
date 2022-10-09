5918a0ab758b80d4aaf1fe9edaef7fe4394e2b15
Hyesung Oh <haesungoh414@gmail.com>
2022-10-09 21:54:55 +0900

    feat: sitemap 생성 자동화

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */ // Default code you can customize according to your requirements.
+
+module.exports = {
+  siteUrl: 'https://www.depromeet.com',
+  generateRobotsTxt: true,
+};
