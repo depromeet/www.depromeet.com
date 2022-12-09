0bf27e2a9491e9f50882ad50cc5c7f34e3f5c798
정대윤 <sensecodevalue@gmail.com>
2022-12-09 22:30:30 +0900

    fix: add number type size check

--- src/hooks/use-media-query/use-media-query.ts
+++ src/hooks/use-media-query/use-media-query.ts
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useState } from 'react';
 
-import { size, SizeKey } from '~/styles/constants';
+import { SIZE, size, SizeKey } from '~/styles/constants';
 
 import { useUserAgent } from '../use-user-agent';
 
@@ -11,7 +11,8 @@ export function useMediaQuery(sizeKey: SizeKey): boolean;
 export default function useMediaQuery(width: number | SizeKey) {
   const { isMobileAgent } = useUserAgent();
   const targetWidth = typeof width === 'number' ? `${width}px` : size[width];
-  const isMobileSize = targetWidth <= size.xs && isMobileAgent;
+  const isMobileSize = (width <= SIZE.xs || targetWidth === size.xs) && isMobileAgent;
+
   const [targetReached, setTargetReached] = useState<boolean>(isMobileSize);
 
   const updateTarget = useCallback((e: MediaQueryListEvent) => {
