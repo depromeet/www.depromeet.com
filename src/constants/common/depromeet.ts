6fa631283f2200e60f09e35325e95a298ffa1704
Hyesung Oh <haesungoh414@gmail.com>
2023-02-23 22:37:16 +0900

    chore: 미사용 상수 제거

--- src/constants/common/depromeet.ts
+++ src/constants/common/depromeet.ts
@@ -6,5 +6,3 @@ export const DEPROMEET_GITHUB = 'https://github.com/depromeet/';
 export const DEPROMEET_BEHANCE = 'https://www.behance.net/Depromeet';
 
 export const KAKAO_PLUS_FRIEND = 'http://pf.kakao.com/_xoxmcxed';
-
-export const EMAIL_13TH_GOOGLE_FORM = 'https://forms.gle/5AwcGJjKT9LUNYfM7';
