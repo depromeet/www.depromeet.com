19b462fe2032b14225a9f5bf337f573c388b82ce
Hyesung Oh <haesungoh414@gmail.com>
2023-03-02 02:00:29 +0900

    feat: lazy motion 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import type { AppContext, AppProps } from 'next/app';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { domMax, LazyMotion } from 'framer-motion';
 
 import Footer from '~/components/common/Footer';
+import GNB from '~/components/common/GNB';
 import { BASE_URL } from '~/constants/common';
 import useRecordPageview from '~/hooks/use-record-pageview';
 import { UserAgentContext } from '~/hooks/use-user-agent';
@@ -19,18 +21,21 @@ export default function App({ Component, pageProps, userAgent }: AppProps & Init
   useRecordPageview();
 
   return (
-    <UserAgentContext.Provider value={userAgent}>
-      <Head>
-        <link rel="canonical" href={currentUrl} />
-        <meta property="og:url" content={currentUrl} />
-      </Head>
+    <LazyMotion features={domMax}>
+      <UserAgentContext.Provider value={userAgent}>
+        <Head>
+          <link rel="canonical" href={currentUrl} />
+          <meta property="og:url" content={currentUrl} />
+        </Head>
 
-      <GlobalStyle />
+        <GlobalStyle />
 
-      <Component {...pageProps} />
+        <GNB />
+        <Component {...pageProps} />
 
-      <Footer />
-    </UserAgentContext.Provider>
+        <Footer />
+      </UserAgentContext.Provider>
+    </LazyMotion>
   );
 }
 App.getInitialProps = async ({ ctx }: AppContext) => {
