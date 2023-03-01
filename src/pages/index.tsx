c40a765913fdf6ed1a21437a0a839a908f7ace7c
Hyesung Oh <haesungoh414@gmail.com>
2023-03-02 00:30:44 +0900

    chore: cursor hover 테스트 코드

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
 import SEO from '~/components/common/SEO';
+import useCursorState from '~/store/cursor/useCursorState';
 
 export default function Root() {
+  const { onMouseEnter, onMouseLeave } = useCursorState();
+
   return (
     <>
       <SEO />
-      <main></main>
+      <main>
+        <div style={{ width: 1000, height: 1000, backgroundColor: 'lightgreen' }}>
+          <button
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+            style={{ width: 100, height: 40, backgroundColor: 'red' }}
+          >
+            button
+          </button>
+        </div>
+      </main>
     </>
   );
 }
