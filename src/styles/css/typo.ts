d56c2a89e8f5eeb2b6d444dc73bacef164325e5a
Hyesung Oh <haesungoh414@gmail.com>
2023-03-02 14:02:36 +0900

    style: 공통 섹션 typo css 작성

---
+++ src/styles/css/typo.ts
@@ -0,0 +1,24 @@
+import { css } from '@emotion/react';
+
+import { colors } from '../constants';
+
+export const sectionSmallCss = css`
+  font-weight: 600;
+  font-size: 0.875rem;
+  line-height: 140%;
+  color: ${colors.gray500};
+`;
+
+export const section36HeadingCss = css`
+  font-weight: 600;
+  font-size: 2.25rem;
+  line-height: 43px;
+  color: ${colors.black};
+`;
+
+export const section40HeadingCss = css`
+  font-weight: 600;
+  font-size: 2.5rem;
+  line-height: 48px;
+  color: ${colors.black};
+`;
