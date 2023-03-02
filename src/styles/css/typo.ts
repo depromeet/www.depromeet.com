d3ab8049fb5956e3930f2d6c67a918ac32fcb222
Hyesung Oh <haesungoh414@gmail.com>
2023-03-02 14:12:01 +0900

    style: 공통 섹션 typo media query 적용

--- src/styles/css/typo.ts
+++ src/styles/css/typo.ts
@@ -1,12 +1,17 @@
 import { css } from '@emotion/react';
 
-import { colors } from '../constants';
+import { colors, mediaQuery } from '../constants';
 
 export const sectionSmallCss = css`
   font-weight: 600;
   font-size: 0.875rem;
   line-height: 140%;
   color: ${colors.gray500};
+
+  ${mediaQuery('xs')} {
+    font-size: 12px;
+    line-height: 14px;
+  }
 `;
 
 export const section36HeadingCss = css`
@@ -14,6 +19,11 @@ export const section36HeadingCss = css`
   font-size: 2.25rem;
   line-height: 43px;
   color: ${colors.black};
+
+  ${mediaQuery('xs')} {
+    font-size: 22px;
+    line-height: 140%;
+  }
 `;
 
 export const section40HeadingCss = css`
@@ -21,4 +31,9 @@ export const section40HeadingCss = css`
   font-size: 2.5rem;
   line-height: 48px;
   color: ${colors.black};
+
+  ${mediaQuery('xs')} {
+    font-size: 22px;
+    line-height: 140%;
+  }
 `;
