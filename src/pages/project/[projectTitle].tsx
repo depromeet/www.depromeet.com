28480d46582f98dba0a9ed41c81738efcb03ff0b
ding-co <pangoons@naver.com>
2023-03-05 01:43:18 +0900

    feat: add pageComponent into pages/project/[projecetTitle]

--- src/pages/project/[projectTitle].tsx
+++ src/pages/project/[projectTitle].tsx
@@ -1,21 +1,23 @@
 import SEO from '~/components/common/SEO';
 import { Project, projects } from '~/components/project/constants';
+import ProjectDetailSection from '~/components/project-detail/ProjectDetailSection/ProjectDetailSection';
 
 interface Props {
   currentProject: Project;
 }
 
 export default function ProjectDetail({ currentProject }: Props) {
+  const { title, description, image } = currentProject;
+
   return (
     <>
       <SEO
-        title={`디프만 - ${currentProject.title}`}
-        description={currentProject.description}
+        title={`디프만 - ${title}`}
+        description={description}
         // TODO: image base url 추가
-        image={`${currentProject.image}`}
+        image={image}
       />
-
-      <main>{currentProject.title}</main>
+      <ProjectDetailSection currentProject={currentProject} />
     </>
   );
 }
