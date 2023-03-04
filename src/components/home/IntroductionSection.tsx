974b012ce96cc18ccce6e0a3880e1859edff2073
Hyesung Oh <haesungoh414@gmail.com>
2023-03-05 04:22:27 +0900

    fix: 론칭 성공률 변경

--- src/components/home/IntroductionSection.tsx
+++ src/components/home/IntroductionSection.tsx
@@ -44,7 +44,7 @@ export default function IntroductionSection() {
             <h3 css={heading3Css}>100%</h3>
             <div css={heading4WrapperCss}>
               <h4 css={heading4Css}>론칭 성공률</h4>
-              <small css={smallCss}>10기, 11기 기준</small>
+              <small css={smallCss}>10기~12기 기준</small>
             </div>
           </div>
         </div>
