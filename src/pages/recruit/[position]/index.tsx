c3854a0a59fcb793a57dbb8b8058183640487676
정대윤 <sensecodevalue@gmail.com>
2023-03-05 21:03:04 +0900

    feat: apply section components in recruit-detail

--- src/pages/recruit/[position]/index.tsx
+++ src/pages/recruit/[position]/index.tsx
@@ -1,5 +1,11 @@
+import ApplySection from '~/components/common/ApplySection';
 import SEO from '~/components/common/SEO';
 import { POSITION_TYPE, PositionType } from '~/components/recruit-detail/constants';
+import DescriptionSection from '~/components/recruit-detail/DescriptionSection';
+import HeaderSection from '~/components/recruit-detail/HeaderSection';
+import PreviousSection from '~/components/recruit-detail/PreviousSection';
+import { sectionCss } from '~/components/recruit-detail/RecruitDetail.style';
+import TipSection from '~/components/recruit-detail/TipSection';
 
 interface Props {
   position: (typeof POSITION_TYPE)[PositionType];
@@ -9,7 +15,13 @@ export default function RecruitDetail({ position }: Props) {
   return (
     <>
       <SEO title={`디프만 - ${position}`} />
-      <main>{position}</main>
+      <main>
+        <HeaderSection positionType={position} />
+        <PreviousSection />
+        <DescriptionSection positionType={position} />
+        <TipSection positionType={position} />
+        <ApplySection wrapperCss={sectionCss} />
+      </main>
     </>
   );
 }
@@ -34,7 +46,6 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }: Paths) {
   const { position } = params;
   const upperCasePosition = position.toUpperCase();
-
   if (!POSITION_TYPE.hasOwnProperty(upperCasePosition)) {
     return { notFound: true };
   }
