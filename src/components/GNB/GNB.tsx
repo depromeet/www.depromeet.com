73332cab285bcc47e3b4935278bc14db87259f8d
99-zziy <wldud8229@gmail.com>
2023-09-09 17:11:09 +0900

    feat: gnb 스타일 추가

--- src/components/GNB/GNB.tsx
+++ src/components/GNB/GNB.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { css } from '@emotion/react';
 
+import { Button } from '~/components/Button';
 import { GNB_MENU_NAME } from '~/constant/gnb';
 import { colors } from '~/styles/colors';
 
@@ -10,13 +11,23 @@ export function GNB() {
   return (
     <nav css={navCss}>
       <div css={navWrapperCss}>
-        {<Image src={LOGO_IMAGE} alt="로고 이미지" width={240} height={30} />}
+        <Link href={'/'}>
+          {<Image src={LOGO_IMAGE} alt="로고 이미지" width={240} height={30} />}
+        </Link>
         <ul css={menuContainerCss}>
           {GNB_MENU_NAME.map(menu => (
-            <li key={menu.name}>
-              <Link css={LinkCss} href={menu.href}>
-                {menu.name}
-              </Link>
+            <li css={menuCss} key={menu.name}>
+              {menu.type === 'button' ? (
+                <Button css={yellow500css}>
+                  <Link css={linkButtonCss} href={menu.href}>
+                    {menu.name}
+                  </Link>
+                </Button>
+              ) : (
+                <Link css={linkCss} href={menu.href}>
+                  {menu.name}
+                </Link>
+              )}
             </li>
           ))}
         </ul>
@@ -49,10 +60,26 @@ const menuContainerCss = css`
   gap: 60px;
 `;
 
-const LinkCss = css`
+const menuCss = css`
+  margin: auto 0;
+`;
+
+const linkCss = css`
   color: ${colors.white};
   font-size: 1.25rem;
   font-weight: 500;
   line-height: 150%; /* 30px */
   letter-spacing: -0.2px;
+`;
+
+const linkButtonCss = css`
+  font-size: 1.25rem;
+  font-weight: 500;
+  line-height: 150%; /* 30px */
+  letter-spacing: -0.2px;
+`;
+
+const yellow500css = css`
+  background-color: ${colors.yellow500};
+  color: ${colors.black800};
 `;
--- src/constant/gnb.ts
+++ src/constant/gnb.ts
@@ -1,19 +1,28 @@
+type GNB = {
+  name: 'About' | '모집안내' | '프로젝트' | '지원하기';
+  href: '/about' | '/recruit' | '/project' | '/';
+  type: 'text' | 'button';
+};
 // TODO: 지원하기 url 넣기
-export const GNB_MENU_NAME = [
+export const GNB_MENU_NAME: GNB[] = [
   {
     name: 'About',
     href: '/about',
+    type: 'text',
   },
   {
     name: '모집안내',
     href: '/recruit',
+    type: 'text',
   },
   {
     name: '프로젝트',
     href: '/project',
+    type: 'text',
   },
   {
     name: '지원하기',
     href: '/',
+    type: 'button',
   },
 ];
