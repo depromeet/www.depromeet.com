5905a6ddffbe17c9bd7761344dfad0ca83600f31
강지영 <62633444+99-zziy@users.noreply.github.com>
2023-09-10 21:52:47 +0900

    [Feat] 버튼 컴포넌트, GNB, Footer 추가 (#253)
    * refactor: 화살표 함수에서 function으로 수정
    
    * feat: global cursor style 삭제
    
    * feat: gnb 추가
    
    * feat: button 추가
    
    * feat: gnb 스타일 추가
    
    * feat: 해당하는 페이지의 버튼 링크 색 변경
    
    * feat: footer 추가
    
    * refactor: function, named export
    
    * feat: footer style 수정

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,77 @@
+import Link from 'next/link';
+import { css } from '@emotion/react';
+
+import { FIRST_ROW_FOOTER_INFOS, SECOND_ROW_FOOTER_INFOS } from '~/constant/footer';
+import { colors } from '~/styles/colors';
+
+export function Footer() {
+  return (
+    <footer css={footerCss}>
+      <div css={footerInfoWrapper}>
+        <ul css={rowCss}>
+          {FIRST_ROW_FOOTER_INFOS.map(footer => (
+            <li key={footer.name}>
+              <Link css={linkCss} href={footer.href}>
+                {footer.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <ul css={[rowCss, rowGapCss]}>
+          {SECOND_ROW_FOOTER_INFOS.map(footer => (
+            <li key={footer.name}>
+              <Link css={linkCss} href={footer.href}>
+                {footer.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <p css={copyrightCss}>© 2023 DEPROMEET. ALL RIGHTS RESERVED.</p>
+      </div>
+    </footer>
+  );
+}
+
+const footerCss = css`
+  background-color: ${colors.black800};
+  padding: 0 20px;
+  width: 100vw;
+`;
+
+const footerInfoWrapper = css`
+  max-width: 1240px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  height: 218px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const rowCss = css`
+  display: flex;
+  gap: 40px;
+`;
+
+const rowGapCss = css`
+  margin-top: 16px;
+`;
+
+const linkCss = css`
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 25.2px; /* 140% */
+  letter-spacing: -0.18px;
+  color: ${colors.gray100};
+`;
+
+const copyrightCss = css`
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 25.2px; /* 140% */
+  letter-spacing: -0.18px;
+  color: ${colors.gray200};
+  margin-top: 40px;
+`;
---
+++ src/constant/gnb.ts
@@ -0,0 +1,28 @@
+export type GNBMenu = {
+  name: 'About' | '모집안내' | '프로젝트' | '지원하기';
+  href: '/about' | '/recruit' | '/project' | '/';
+  type: 'text' | 'button';
+};
+// TODO: 지원하기 url 넣기
+export const GNB_MENU_NAME: GNBMenu[] = [
+  {
+    name: 'About',
+    href: '/about',
+    type: 'text',
+  },
+  {
+    name: '모집안내',
+    href: '/recruit',
+    type: 'text',
+  },
+  {
+    name: '프로젝트',
+    href: '/project',
+    type: 'text',
+  },
+  {
+    name: '지원하기',
+    href: '/',
+    type: 'button',
+  },
+];
