f8ea21a0d84c259c9ec8863312b4094ee8fd61d7
yunho <kimyouknow@naver.com>
2023-09-17 16:57:51 +0900

    refactor: 모집직군 컴포넌트

--- src/components/Positions/Positions.stories.tsx
+++ src/components/Positions/Positions.stories.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps } from 'react';
+import { css } from '@emotion/react';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Positions } from './index';
@@ -13,22 +13,14 @@ export default meta;
 
 type Story = StoryObj<typeof Positions>;
 
-const MOCK: Array<ComponentProps<typeof Positions.Item>> = [
-  { type: 'design', title: 'UX/UI DESIGN', link: '' },
-  { type: 'ios', title: 'IOS', link: '' },
-  { type: 'aos', title: 'ANDROID', link: '' },
-  { type: 'web', title: 'WEB', link: '' },
-  { type: 'server', title: 'SERVER', link: '' },
-];
-
 export const Primary: Story = {
   render: () => (
-    <Positions>
-      <Positions.Body>
-        {MOCK.map(({ ...info }) => (
-          <Positions.Item key={info.type} {...info} />
-        ))}
-      </Positions.Body>
-    </Positions>
+    <div
+      css={css`
+        background-color: #070814;
+      `}
+    >
+      <Positions />,
+    </div>
   ),
 };
