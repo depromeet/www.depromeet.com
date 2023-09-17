0a8d9ac0dad26fd1d5d02346732591fb27c8f34e
99-zziy <wldud8229@gmail.com>
2023-09-17 21:38:20 +0900

    feat: 프로젝트 추가

--- src/constant/project.ts
+++ src/constant/project.ts
@@ -104,4 +104,151 @@ export const PROJECT_LIST = [
       { type: 'Web', href: 'jalingobi.com' },
     ],
   },
+  {
+    title: '짝심삼일',
+    subTitle: '12기',
+    description: '나를 바꾸는 작은 습관의 힘,<br/>습관 관리 서비스',
+    links: [
+      {
+        type: 'Behance',
+        href: 'https://url.kr/pyhgze',
+      },
+      { type: 'Github', href: 'https://github.com/depromeet12th/three-days-android' },
+      { type: 'App', href: 'https://c11.kr/19ln2' },
+    ],
+  },
+  {
+    title: 'KNOCKNOCK',
+    subTitle: '12기',
+    description: '내 친구가 보내는 생생한<br/>푸시알림 서비스',
+    links: [
+      {
+        type: 'Behance',
+        href: 'https://www.behance.net/gallery/161848521/KNOCKNOCK-PUSH-NOTIFICATION-CUSTOM-SERVICE',
+      },
+      { type: 'Github', href: 'https://github.com/depromeet/12th-KnockKnock-Android' },
+      {
+        type: 'App',
+        href: 'https://play.google.com/store/apps/details?id=com.depromeet.knockknock',
+      },
+    ],
+  },
+  {
+    title: 'TICLEMOA',
+    subTitle: '12기',
+    description: '아티클을 모아 지식을 태산처럼,<br/>아티클 스크랩 서비스',
+    links: [
+      {
+        type: 'Behance',
+        href: 'https://www.behance.net/gallery/161983453/TICLEMOA-Article-clipping-service-',
+      },
+      { type: 'Github', href: 'https://github.com/depromeet/ticlemoa-backend' },
+      {
+        type: 'App',
+        href: 'https://apps.apple.com/kr/app/ticlemoa/id1659267166',
+      },
+    ],
+  },
+  {
+    title: '똑스',
+    subTitle: '12기',
+    description: '내가 만든 퀴즈로<br/>스터디를 재미있게, 똑스',
+    links: [
+      {
+        type: 'Behance',
+        href: 'https://www.behance.net/gallery/161231589/Toks-Quiz-Study-Service-',
+      },
+      { type: 'Github', href: 'https://github.com/depromeet/toks-web' },
+      {
+        type: 'Web',
+        href: 'https://tokstudy.com/',
+      },
+    ],
+  },
+  {
+    title: '코퀄리티',
+    subTitle: '12기',
+    description: '지식을 공유하고, 후원하며<br/>함께 성장하는 블로깅 플랫폼',
+    links: [
+      {
+        type: 'Behance',
+        href: 'https://www.behance.net/gallery/161841129/-Coquality-High-Quality-Blogging-',
+      },
+      {
+        type: 'Web',
+        href: 'coquality.vercel.app',
+      },
+    ],
+  },
+  {
+    title: '아맞다',
+    subTitle: '12기',
+    description: '대신 외쳐주는 소지품 리스트',
+    links: [
+      {
+        type: 'Behance',
+        href: 'https://www.behance.net/gallery/161161601/-Ahmatda-Checklist-for-Your-Belongings',
+      },
+      {
+        type: 'Web',
+        href: 'https://ahmatda.notion.site/ahmatda/3202c2a4e2dd440eb95ae3345a130fc4',
+      },
+    ],
+  },
+  {
+    title: 'PING-PONG!',
+    subTitle: '12기',
+    description: '재능을 교환하고 나누며<br/>성장하는 플랫폼, 핑퐁',
+    links: [
+      {
+        type: 'Behance',
+        href: 'https://www.behance.net/gallery/161783411/Ping-Pong',
+      },
+      {
+        type: 'App',
+        href: 'https://apps.apple.com/kr/app/%ED%95%91%ED%90%81-pingpong/id1662351621',
+      },
+    ],
+  },
+  {
+    title: '꼬깃',
+    subTitle: '12기',
+    description: '진심을 표현하고 싶을 때,<br/>꼬깃 접어 전해보세요',
+    links: [
+      {
+        type: 'Behance',
+        href: 'https://url.kr/qd3ijo',
+      },
+      {
+        type: 'Web',
+        href: 'https://www.ggo-geet.com',
+      },
+    ],
+  },
+  {
+    title: '비어에어',
+    subTitle: '11기',
+    description: '편의점 세계 맥주로<br/>세계 여행도장깨기',
+    links: [
+      {
+        type: 'Behance',
+        href: 'https://www.behance.net/gallery/147281821/Beer-Air-UXUI-Design-Mobile-App-Servic',
+      },
+      {
+        type: 'Web',
+        href: 'https://beerair.kr',
+      },
+    ],
+  },
+  {
+    title: 'noonbody',
+    subTitle: '10기',
+    description: '건강한 눈바디 다이어트',
+    links: [
+      {
+        type: 'App',
+        href: 'https://play.google.com/store/apps/details?id=com.def.everybody_android',
+      },
+    ],
+  },
 ];
