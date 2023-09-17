0475cc5111d391e1b55d42beb20ff0f32a777500
sumi-0011 <selina2000@naver.com>
2023-09-17 19:11:36 +0900

    feat: 모집 마감 버튼 추가

--- src/components/TimerContainer/TimerContainer.tsx
+++ src/components/TimerContainer/TimerContainer.tsx
@@ -7,13 +7,16 @@ export function TimerContainer() {
     <div css={containerCss}>
       <div css={gradientCss} />
       <div css={layoutCss}>
-        <div css={headingCss}>
-          <h1>DEPROMEET</h1>
-          <p>디프만은 디자이너와 개발자가 만나 서비스 기획부터 론칭까지</p>
-          <p>하나의 프로덕트를 완성하며 성장하는 IT 커뮤니티입니다</p>
-        </div>
         <div>
+          <div css={headingCss}>
+            <h1>DEPROMEET</h1>
+            <p>디프만은 디자이너와 개발자가 만나 서비스 기획부터 론칭까지</p>
+            <p>하나의 프로덕트를 완성하며 성장하는 IT 커뮤니티입니다</p>
+          </div>
+          {/* <div> */}
           <Timer deadlineDay={'2023-09-18'} />
+          {/* </div> */}
+          <button css={buttonCss}>14기 지원하기</button>
         </div>
       </div>
     </div>
@@ -37,6 +40,15 @@ const layoutCss = css`
   height: 768.857px;
   position: relative;
   z-index: 1; // NOTE : gradient 뒤로 가려지지 않게
+
+  & > div {
+    max-width: 726px;
+    margin: 0 auto;
+
+    & > * {
+      margin-bottom: 20px;
+    }
+  }
 `;
 
 const gradientCss = css`
@@ -52,7 +64,8 @@ const gradientCss = css`
 const headingCss = (theme: Theme) => css`
   text-align: center;
   padding-top: 147px;
-  padding-bottom: 40px;
+  padding-bottom: 20px;
+
   & > h1 {
     ${theme.typos.decimal.title1}
     color: ${theme.colors.white};
@@ -63,4 +76,14 @@ const headingCss = (theme: Theme) => css`
     ${theme.typos.pretendard.subTitle2};
     color: ${theme.colors.gray20};
   }
+`;
+
+const buttonCss = (theme: Theme) => css`
+  ${theme.typos.pretendard.body1};
+  color: ${theme.colors.black800};
+  background-color: ${theme.colors.yellow500};
+  width: 100%;
+
+  padding: 16px 24px;
+  height: 58px;
 `;
