02284979f97eadda1fbbec7999ff1509b5a3d4b0
sumi.byun <selina2000@naver.com>
2023-09-17 21:43:14 +0900

    [Feat] Main 페이지 최상단 (#271)
    * feat: main image add
    
    * feat: timer add
    
    * feat: 모집 마감 버튼 추가
    
    * feat: 모집 마감 날짜 지정
    
    * fix: hidration error solve
    
    * feat: todo 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
+import { css, Theme } from '@emotion/react';
+
 import { SEO } from '~/components/SEO';
+import { TimerContainer } from '~/components/TimerContainer';
 
 export default function Root() {
   return (
     <>
       <SEO />
-      <main></main>
+      <main css={layoutCss}>
+        <TimerContainer />
+      </main>
     </>
   );
 }
+
+const layoutCss = (theme: Theme) => css`
+  background-color: ${theme.colors.black800};
+`;
