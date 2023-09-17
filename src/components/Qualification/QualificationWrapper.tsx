2866198c41f503248cd1c76141ebe0f779f94399
yunho <kimyouknow@naver.com>
2023-09-17 16:50:50 +0900

    refactor: 지원자격 컴포넌트

--- src/components/Qualification/QualificationWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-interface QualificationWrapperProps {}
-
-export function QualificationWrapper({ children }: PropsWithChildren<QualificationWrapperProps>) {
-  return <div>{children}</div>;
-}
