89e160ce652edc708dc2ca5ae085a733bc225e37
99-zziy <wldud8229@gmail.com>
2023-09-17 21:13:05 +0900

    feat: 13기 프로젝트 데이터 추가

---
+++ src/constant/project.ts
@@ -0,0 +1,107 @@
+export const TAB_LIST = ['전체', '13기', '12기', '11기', '-10기'];
+export const PROJECT_LIST = [
+  {
+    title: '자린고비',
+    subTitle: '13기',
+    description: '거지들의 이야기로 쌓이는<br/>소비습관 개선 서비스',
+    links: [
+      {
+        type: 'Behance',
+        href: 'https://www.behance.net/gallery/175690517/-Improve-your-spending-habit-with-people',
+      },
+      { type: 'Github', href: 'https://github.com/depromeet/jalingobi-client' },
+      { type: 'Web', href: 'jalingobi.com' },
+    ],
+  },
+  {
+    title: '오버스윗',
+    subTitle: '13기',
+    description: '카페 음료의 당을<br/>기록하고 관리하는 서비스',
+    links: [
+      {
+        type: 'Behance',
+        href: 'https://www.behance.net/gallery/175690517/-Improve-your-spending-habit-with-people',
+      },
+      { type: 'Github', href: 'https://github.com/depromeet/jalingobi-client' },
+      { type: 'Web', href: 'jalingobi.com' },
+    ],
+  },
+  {
+    title: 'Na Lab(나랩)',
+    subTitle: '13기',
+    description: '동료의 익명 피드백으로<br/>발견하는 나만의 커리어 DNA',
+    links: [
+      {
+        type: 'Behance',
+        href: 'https://www.behance.net/gallery/175690517/-Improve-your-spending-habit-with-people',
+      },
+      { type: 'Github', href: 'https://github.com/depromeet/jalingobi-client' },
+      { type: 'Web', href: 'jalingobi.com' },
+    ],
+  },
+  {
+    title: '인사이트아웃',
+    subTitle: '13기',
+    description: 'AI 기반 직무역량 키워드 추천 및<br/>자기소개서 작성 서비스',
+    links: [
+      {
+        type: 'Behance',
+        href: 'https://www.behance.net/gallery/175690517/-Improve-your-spending-habit-with-people',
+      },
+      { type: 'Github', href: 'https://github.com/depromeet/jalingobi-client' },
+      { type: 'Web', href: 'jalingobi.com' },
+    ],
+  },
+  {
+    title: 'Street Drop',
+    subTitle: '13기',
+    description: '길을 걸으며 음악을 드랍하는<br/>음악 공유 소셜 서비스',
+    links: [
+      {
+        type: 'Behance',
+        href: 'https://www.behance.net/gallery/175690517/-Improve-your-spending-habit-with-people',
+      },
+      { type: 'Github', href: 'https://github.com/depromeet/jalingobi-client' },
+      { type: 'Web', href: 'jalingobi.com' },
+    ],
+  },
+  {
+    title: 'Whatnow (왔나)',
+    subTitle: '13기',
+    description: '약속 가는 중 친구들과 즐기는<br/>Share-play 서비스',
+    links: [
+      {
+        type: 'Behance',
+        href: 'https://www.behance.net/gallery/175690517/-Improve-your-spending-habit-with-people',
+      },
+      { type: 'Github', href: 'https://github.com/depromeet/jalingobi-client' },
+      { type: 'Web', href: 'jalingobi.com' },
+    ],
+  },
+  {
+    title: 'Ding-Dong (딩동)',
+    subTitle: '13기',
+    description: '서로의 TMI를 공유하고<br/>친해지고 싶은 마음을 전하세요!',
+    links: [
+      {
+        type: 'Behance',
+        href: 'https://www.behance.net/gallery/175690517/-Improve-your-spending-habit-with-people',
+      },
+      { type: 'Github', href: 'https://github.com/depromeet/jalingobi-client' },
+      { type: 'Web', href: 'jalingobi.com' },
+    ],
+  },
+  {
+    title: 'Pumping (펌핑)',
+    subTitle: '13기',
+    description: '크루원과 함께<br/>운동 경쟁하는 서비스',
+    links: [
+      {
+        type: 'Behance',
+        href: 'https://www.behance.net/gallery/175690517/-Improve-your-spending-habit-with-people',
+      },
+      { type: 'Github', href: 'https://github.com/depromeet/jalingobi-client' },
+      { type: 'Web', href: 'jalingobi.com' },
+    ],
+  },
+];
