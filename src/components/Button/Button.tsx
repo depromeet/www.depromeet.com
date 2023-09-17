6c42318cb29516e4dad572feec54d9ec6f7b684d
sumi.byun <selina2000@naver.com>
2023-09-17 23:19:31 +0900

    [Feat] 버튼 컴포넌트 수정 (#276)
    * feat: 버튼 사이즈 추가, disabled 추가
    
    * feat: active hover 상태 추가
    
    * fix: disabled 순서 변경
    
    * [Feat] breakpoint theme 추가 (#273)
    
    * feat: breakpoint add
    
    * feat: mobile button 추가
    
    * feat: media query 이전 기수 처럼 변경

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,62 @@
-import { ButtonHTMLAttributes, PropsWithChildren } from 'react';
-import { css } from '@emotion/react';
+import { ButtonHTMLAttributes } from 'react';
+import { css, Interpolation, Theme } from '@emotion/react';
 
-export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & PropsWithChildren;
+import { mediaQuery } from '~/styles/media';
 
-export function Button({ children, ...props }: ButtonProps) {
+type ButtonSize = 'md' | 'lg';
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: ButtonSize;
+  overrideCss?: Interpolation<Theme>;
+}
+
+export function Button({ children, size = 'md', overrideCss, ...props }: ButtonProps) {
   return (
-    <button {...props} css={buttonCss}>
+    <button {...props} css={theme => [buttonCss(theme, size), overrideCss]}>
       {children}
     </button>
   );
 }
 
-const buttonCss = css`
-  padding: 12px 32px;
-  font-size: 20px;
-  height: 54px;
+const buttonCss = (theme: Theme, size: ButtonSize) => css`
+  ${theme.typos.pretendard.body1};
+  background-color: ${theme.colors.yellow500};
+  color: ${theme.colors.black800};
+  display: block;
+
+  ${size === 'md' &&
+  css`
+    padding: 0 24px;
+    height: 42px;
+
+    ${mediaQuery('mobile')} {
+      font-size: 14px;
+      height: 34px;
+    }
+  `}
+
+  ${size === 'lg' &&
+  css`
+    width: 100%;
+    padding: 0 24px;
+    height: 54px;
+
+    ${mediaQuery('mobile')} {
+      ${theme.typos.pretendard.body2};
+      height: 42px;
+    }
+  `}
+
+
+  &:hover {
+    background-color: ${theme.colors.yellow400};
+  }
+
+  &:active {
+    background-color: ${theme.colors.yellow300};
+  }
+
+  &:disabled {
+    background-color: ${theme.colors.gray300};
+    color: ${theme.colors.gray100};
+  }
 `;
