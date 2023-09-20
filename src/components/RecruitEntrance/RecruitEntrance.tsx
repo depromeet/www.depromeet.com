c3fb6dabc598fd1ce46ee8d97d19db9ef1a52608
sumi-0011 <selina2000@naver.com>
2023-09-20 21:25:40 +0900

    refactor: css 분리

--- src/components/RecruitEntrance/RecruitEntrance.tsx
+++ src/components/RecruitEntrance/RecruitEntrance.tsx
@@ -8,7 +8,7 @@ import { mediaQuery } from '~/styles/media';
 export function RecruitEntrance() {
   return (
     <section css={[commonLayoutCss, layoutCss]}>
-      <div>
+      <div css={imageContainerCss}>
         <Image
           src="/images/main/coin-drop.png"
           width={400}
@@ -16,7 +16,7 @@ export function RecruitEntrance() {
           alt="passport combination"
         />
       </div>
-      <div>
+      <div css={infoContainerCss}>
         <h1>14기 모집 안내</h1>
         <div>
           <p>지원 자격부터 모집 직무까지</p>
@@ -28,75 +28,76 @@ export function RecruitEntrance() {
   );
 }
 
-const layoutCss = (theme: Theme) => css`
-  display: flex;
-  gap: 80px;
-  & > div:first-of-type {
-    padding: 24px;
-  }
-
-  & > div:last-of-type {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    gap: 24px;
-    text-align: left;
-    padding-right: 80px;
-
-    h1 {
-      ${theme.typos.pretendard.subTitle1};
-      color: ${theme.colors.white};
-    }
-    p {
-      ${theme.typos.pretendard.subTitle2};
-      color: ${theme.colors.gray100};
-    }
-  }
-
+const imageContainerCss = css`
+  padding: 24px;
   ${mediaQuery('tablet')} {
-    gap: 7px;
-
-    & > div:first-of-type {
-      width: 346px;
-      padding: 0;
-      margin-left: 53px;
-      img {
-        position: relative;
-        left: -53px;
-      }
-    }
-
-    & > div:last-of-type {
-      padding-right: 0;
+    width: 346px;
+    padding: 0;
+    margin-left: 53px;
+    img {
+      position: relative;
+      left: -53px;
     }
   }
 
   ${mediaQuery('mobile')} {
+    width: 124px;
+    margin-left: 0;
+    img {
+      width: auto;
+      height: 170px;
+      left: -16px;
+    }
+  }
+`;
+
+const infoContainerCss = (theme: Theme) => css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 24px;
+  text-align: left;
+  padding-right: 80px;
+
+  h1 {
+    ${theme.typos.pretendard.subTitle1};
+    color: ${theme.colors.white};
+  }
+  p {
+    ${theme.typos.pretendard.subTitle2};
+    color: ${theme.colors.gray100};
+  }
+
+  ${mediaQuery('tablet')} {
+    padding-right: 0;
+  }
+
+  ${mediaQuery('mobile')} {
+    gap: 8px;
+
+    h1 {
+      ${theme.typos.pretendard.subTitle2};
+    }
+
+    p {
+      font-size: 14px;
+    }
+    button {
+      margin-top: 16px;
+    }
+  }
+`;
+
+const layoutCss = css`
+  display: flex;
+  gap: 80px;
+
+  ${mediaQuery('tablet')} {
+    gap: 7px;
+  }
+
+  ${mediaQuery('mobile')} {
     gap: 16px;
-
-    & > div:first-of-type {
-      width: 124px;
-      margin-left: 0;
-      img {
-        width: auto;
-        height: 170px;
-        left: -16px;
-      }
-    }
-    & > div:last-of-type {
-      gap: 8px;
-
-      h1 {
-        ${theme.typos.pretendard.subTitle2};
-      }
-
-      p {
-        font-size: 14px;
-      }
-      button {
-        margin-top: 16px;
-      }
-    }
   }
 `;
