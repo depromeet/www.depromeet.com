bc314e00a50263ba9641538ac9b8cead11534841
byun sumi <sumi@adenasoft.com>
2023-09-21 11:19:18 +0900

    feat: main sign image 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { css, Theme } from '@emotion/react';
 
 import { FAQ } from '~/components/FAQ';
@@ -8,6 +9,7 @@ import { SectionTitle } from '~/components/SectionTitle';
 import { SEO } from '~/components/SEO';
 import { TimerContainer } from '~/components/TimerContainer';
 import { MEMBER_SCHEDULE, SESSION_SCHEDULES } from '~/constant/schedule';
+import { commonLayoutCss } from '~/styles/layout';
 import { mediaQuery } from '~/styles/media';
 
 export default function Root() {
@@ -26,6 +28,9 @@ export default function Root() {
           </section>
           <section>
             <FAQ />
+          </section>
+          <section css={[commonLayoutCss, signImageContainerCss]}>
+            <Image src="/images/sign/main.png" fill alt="DPM 14th" />
           </section>
         </div>
       </main>
@@ -46,5 +51,22 @@ const contentCss = css`
     & > section {
       margin-bottom: 120px;
     }
+  }
+`;
+
+const signImageContainerCss = css`
+  position: relative;
+  width: 100%;
+  height: 356px;
+  img {
+    width: 100%;
+    object-fit: contain;
+  }
+
+  ${mediaQuery('tablet')} {
+    height: 262px;
+  }
+  ${mediaQuery('mobile')} {
+    height: 122px;
   }
 `;
