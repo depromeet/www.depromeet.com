ce54247c75af79837dc3b6b4d0323b16d2009a3a
yunho <kimyouknow@naver.com>
2023-09-23 14:33:18 +0900

    feat: contact component

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,55 @@
+import Link from 'next/link';
+import { css, Theme } from '@emotion/react';
+
+import { SectionTitle } from '~/components/SectionTitle';
+import { CONTACT_INFO } from '~/constant/contactInfo';
+import { commonLayoutCss } from '~/styles/layout';
+import { mediaQuery } from '~/styles/media';
+
+export function Contact() {
+  return (
+    <div css={[commonLayoutCss, layoutCss]}>
+      <SectionTitle label="Contact" title="디프만에 대해 궁금한게 있으신가요?" />
+      <ul css={infoListCss}>
+        {CONTACT_INFO.map(footer => (
+          <li key={footer.name}>
+            <Link css={InfoCss} href={footer.href} target="_blank">
+              <span css={infoNameCss}>{footer.name}</span>
+              <span>{footer.detail}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+const layoutCss = css``;
+
+const infoListCss = css`
+  display: flex;
+  gap: 20px;
+`;
+
+const InfoCss = (theme: Theme) => css`
+  ${theme.typos.pretendard.body1};
+  background-color: ${theme.colors.black400};
+  color: ${theme.colors.gray20};
+  width: 470px;
+  height: 172px;
+  padding: 48px 36px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  gap: 20px;
+
+  ${mediaQuery('mobile')} {
+    width: 344px;
+  }
+`;
+
+const infoNameCss = (theme: Theme) => css`
+  ${theme.typos.decimal.subTitle1};
+  color: ${theme.colors.blue300};
+`;
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,13 @@
+import { Contact } from '~/components/Contact';
 import { SEO } from '~/components/SEO';
 
 export default function About() {
   return (
     <>
       <SEO title="디프만 - About" />
-      <main></main>
+      <main>
+        <Contact />
+      </main>
     </>
   );
 }
