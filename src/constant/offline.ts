8535e1e47f5b3b30943e06ae9e4b69ee3c5fa61b
yunho <kimyouknow@naver.com>
2023-09-23 16:19:50 +0900

    feat: 오프라인 세션

---
+++ src/constant/offline.ts
@@ -0,0 +1,38 @@
+export const OFFLINE_SESSIONS = [
+  {
+    title: '오리엔테이션',
+    subTitle: 'Orientation',
+    description: '디프만의 첫 시작, 서로를 알아갈 수 있는 오리엔테이션에 모두가 함께 해요.',
+    img: '/images/session/orientation.png',
+  },
+  {
+    title: '네트워킹 데이',
+    subTitle: 'Networking Day',
+    description: '다른 팀, 직군의 멤버들을 만나요. TMI부터 커리어 토크까지 친해지는 시간을 가져요.',
+    img: '/images/session/networking.png',
+  },
+  {
+    title: '사용성 테스트',
+    subTitle: 'Usability Test',
+    description: '사용성 테스트를 통해 문제를 발견하고 개선하여 더 가치있는 서비스를 만들어요.',
+    img: '/images/session/usability.png',
+  },
+  {
+    title: '중간발표',
+    subTitle: 'Midterm announce',
+    description: '각 팀의 진행 상황을 공유하며 실질적ㅇ니 피드백을 주고 받을 수 있어요',
+    img: '/images/session/midterm.png',
+  },
+  {
+    title: '론칭데이',
+    subTitle: 'Launching Day',
+    description: '론칭된 서비스를 팀 부스에서 공개해요. 모든 팀의 서비스를 사용해볼 수 있어요.',
+    img: '/images/session/launching.png',
+  },
+  {
+    title: '최종발표',
+    subTitle: 'Final announce',
+    description: '서비스의 성과와 그 과정을 공유 및 회고하며 프로젝트를 마무리해요.',
+    img: '/images/session/final.png',
+  },
+];
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,6 @@
 import { AboutInfo } from '~/components/AboutInfo';
 import { Contact } from '~/components/Contact';
+import { OfflineSession } from '~/components/OfflineSession';
 import { SEO } from '~/components/SEO';
 
 export default function About() {
@@ -8,6 +9,7 @@ export default function About() {
       <SEO title="디프만 - About" />
       <main>
         <AboutInfo />
+        <OfflineSession />
         <Contact />
       </main>
     </>
