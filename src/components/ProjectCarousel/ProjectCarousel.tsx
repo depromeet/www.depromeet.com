0cd5eeb904c73560986420416f62a603cf40e989
sumi-0011 <selina2000@naver.com>
2023-09-29 00:26:29 +0900

    [Feat] Project 13기만 보여주기

--- src/components/ProjectCarousel/ProjectCarousel.tsx
+++ src/components/ProjectCarousel/ProjectCarousel.tsx
@@ -5,12 +5,14 @@ import { ProjectCarouselItem } from '~/components/ProjectCarousel/ProjectCarouse
 import { SectionTitle } from '~/components/SectionTitle';
 import { PROJECT_LIST } from '~/constant/project';
 
+const PROJECT_13 = PROJECT_LIST.filter(project => project.subTitle === '13기');
+
 export function ProjectCarousel() {
   return (
     <section css={layoutCss}>
       <SectionTitle label="Project" title="프로젝트" />
       <Marquee>
-        {PROJECT_LIST.map(project => (
+        {PROJECT_13.map(project => (
           <ProjectCarouselItem key={project.title} {...project} />
         ))}
       </Marquee>
