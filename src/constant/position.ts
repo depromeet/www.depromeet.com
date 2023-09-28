408e765d3e2efe8d5a85cfecbbea8bf51fb55616
sumi-0011 <selina2000@naver.com>
2023-09-28 22:50:58 +0900

    feat: 모집 직군 config link 연결

--- src/constant/position.ts
+++ src/constant/position.ts
@@ -3,9 +3,9 @@ import { ComponentProps } from 'react';
 import { PositionsItem } from '~/components/Positions/PositionsItem';
 
 export const POSITIONS: Array<ComponentProps<typeof PositionsItem>> = [
-  { type: 'design', title: 'UX/UI DESIGN', link: '' },
-  { type: 'ios', title: 'IOS', link: '' },
-  { type: 'aos', title: 'ANDROID', link: '' },
-  { type: 'web', title: 'WEB', link: '' },
-  { type: 'server', title: 'SERVER', link: '' },
+  { type: 'design', title: 'UX/UI DESIGN', link: process.env.NEXT_PUBLIC_GREETING_DESIGN ?? '' },
+  { type: 'ios', title: 'IOS', link: process.env.NEXT_PUBLIC_GREETING_IOS ?? '' },
+  { type: 'aos', title: 'ANDROID', link: process.env.NEXT_PUBLIC_GREETING_ANDROID ?? '' },
+  { type: 'web', title: 'WEB', link: process.env.NEXT_PUBLIC_GREETING_WEB ?? '' },
+  { type: 'server', title: 'SERVER', link: process.env.NEXT_PUBLIC_GREETING_SERVER ?? '' },
 ];
