79b3c346354fc905b18214b804f8d60e249ec57d
yunho <kimyouknow@naver.com>
2023-09-28 16:38:44 +0900

    chore: pxToRem -> util

--- src/components/AboutInfo/AboutInfo.tsx
+++ src/components/AboutInfo/AboutInfo.tsx
@@ -3,7 +3,7 @@ import { css, Theme } from '@emotion/react';
 
 import { commonLayoutCss } from '~/styles/layout';
 import { mediaQuery } from '~/styles/media';
-import { pxToRem } from '~/styles/typo';
+import { pxToRem } from '~/styles/style.utils';
 
 export function AboutInfo() {
   return (
---
+++ src/styles/style.utils.ts
@@ -0,0 +1,1 @@
+export const pxToRem = (px: number, base = 16) => `${px / base}rem`;
