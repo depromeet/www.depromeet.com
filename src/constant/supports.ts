02f54553749dbc17f17bf11d276367f9082be95a
yunho <kimyouknow@naver.com>
2023-09-29 18:08:44 +0900

    chore: 후원사 소개 워딩 수정

--- src/constant/supports.ts
+++ src/constant/supports.ts
@@ -18,13 +18,13 @@ export const SUPPORTS: Support[] = [
   {
     title: '인프런',
     subTitle: 'Inflearn',
-    description: '인프런 강의 30% 할인쿠폰과 강의 수강을<br/> 위해 총 48만 포인트를 제공해요',
+    description: '인프런 강의 30% 할인쿠폰과 강의 수강을<br/> 위해 총 48만 포인트를 제공합니다.',
     img: '/images/support/inflearn.png',
   },
   {
     title: '모두의연구소',
     subTitle: 'Modulabs',
-    description: '오프라인 세션 운영을 위한<br/>공간을 지원해요',
+    description: '오프라인 세션 운영을 위한<br/>공간을 지원합니다.',
     img: '/images/support/모두의연구소.png',
   },
 ];
