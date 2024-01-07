f1a2fbadc8616938e33b43e7e092a7328095f1b9
kimyouknow <kimyouknow@naver.com>
2024-01-07 23:47:45 +0900

    refactor: 음수 및 정수가 아닌 숫자 에러처리

--- src/utils/format.spec.ts
+++ src/utils/format.spec.ts
@@ -7,6 +7,7 @@ describe('format 관련 유틸 함수', () => {
     it('🟢 한 자리 양수에 대해 선행 0이 있는 문자열을 반환한다. ', () => {
       expect(formatSingleDigit(5)).toBe('05');
       expect(formatSingleDigit(9)).toBe('09');
+      expect(formatSingleDigit(1.0)).toBe('01');
     });
     it('🟢 "0"에 대해 선행0이 있는 문자열을 반환한다.', () => {
       expect(formatSingleDigit(0)).toBe('00');
@@ -18,14 +19,24 @@ describe('format 관련 유틸 함수', () => {
       expect(formatSingleDigit(100)).toBe('100');
       expect(formatSingleDigit(9999999)).toBe('9999999');
     });
-
-    it('🟢 음수는 그대로 반환한다.', () => {
-      expect(formatSingleDigit(-5)).toBe('-5');
-      expect(formatSingleDigit(-9)).toBe('-9');
+    it('🔴 음수에 대해 에러를 반환한다..', () => {
+      expect(() => formatSingleDigit(-5)).toThrowError(
+        '잘못된 입력입니다. 양수 정수만 처리 가능합니다.'
+      );
+      expect(() => formatSingleDigit(-9)).toThrowError(
+        '잘못된 입력입니다. 양수 정수만 처리 가능합니다.'
+      );
+      expect(() => formatSingleDigit(-9.1)).toThrowError(
+        '잘못된 입력입니다. 양수 정수만 처리 가능합니다.'
+      );
     });
-    it('🟢 정수가 아닌 숫자는 그대로 반환한다.', () => {
-      expect(formatSingleDigit(5.5)).toBe('5.5');
-      expect(formatSingleDigit(9.9)).toBe('9.9');
+    it('🔴 정수가 아닌 숫자에 대해 에러를 반환한다.', () => {
+      expect(() => formatSingleDigit(5.5)).toThrowError(
+        '잘못된 입력입니다. 양수 정수만 처리 가능합니다.'
+      );
+      expect(() => formatSingleDigit(9.9)).toThrowError(
+        '잘못된 입력입니다. 양수 정수만 처리 가능합니다.'
+      );
     });
   });
 });
--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,10 +1,6 @@
 export function formatSingleDigit(number: number) {
-  if (number < 0) {
-    return number.toString();
-  }
-
-  if (!Number.isInteger(number)) {
-    return number.toString();
+  if (number < 0 || !Number.isInteger(number)) {
+    throw new Error('잘못된 입력입니다. 양수 정수만 처리 가능합니다.');
   }
 
   if (number < 10) {
