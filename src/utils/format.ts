c4e07a9c19864f2de24bfee2ecadafa3479fe5d5
kimyouknow <kimyouknow@naver.com>
2024-01-07 23:34:45 +0900

    feat: formatSingleDigit 선행조건 강화

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,4 +1,12 @@
 export function formatSingleDigit(number: number) {
+  if (number < 0) {
+    return number.toString();
+  }
+
+  if (!Number.isInteger(number)) {
+    return number.toString();
+  }
+
   if (number < 10) {
     return '0' + number;
   }
