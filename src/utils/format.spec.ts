902a6917146a3bd654249088a43bef6b2245fcb2
kimyouknow <kimyouknow@naver.com>
2024-01-07 23:34:25 +0900

    feat: 테스트 케이스 추가

--- src/utils/format.spec.ts
+++ src/utils/format.spec.ts
@@ -4,23 +4,26 @@ import { formatSingleDigit } from '~/utils/format';
 
 describe('format 관련 유틸 함수', () => {
   describe('formatSingleDigit 함수 테스트', () => {
-    it('🟢 한 자리 숫자에 대해 선행 0이 있는 문자열을 반환한다. ', () => {
-      expect(formatSingleDigit(0)).toBe('00');
+    it('🟢 한 자리 양수에 대해 선행 0이 있는 문자열을 반환한다. ', () => {
       expect(formatSingleDigit(5)).toBe('05');
       expect(formatSingleDigit(9)).toBe('09');
     });
-    it('🟢 두 자리 숫자에 대해 숫자를 문자열로 반환한다.', () => {
+    it('🟢 "0"에 대해 선행0이 있는 문자열을 반환한다.', () => {
+      expect(formatSingleDigit(0)).toBe('00');
+    });
+    it('🟢 두 자리 이상의 숫자에 대해 해당 숫자를 문자열로 반환한다.', () => {
       expect(formatSingleDigit(10)).toBe('10');
       expect(formatSingleDigit(25)).toBe('25');
       expect(formatSingleDigit(99)).toBe('99');
+      expect(formatSingleDigit(100)).toBe('100');
+      expect(formatSingleDigit(9999999)).toBe('9999999');
     });
-    // TODO: @kimyouknow 아래 테스트 케이스에 맞게 formatSingleDigit 수정하기
-    it.todo('🟢 음수는 그대로 반환한다.', () => {
+
+    it('🟢 음수는 그대로 반환한다.', () => {
       expect(formatSingleDigit(-5)).toBe('-5');
       expect(formatSingleDigit(-9)).toBe('-9');
     });
-    // TODO: @kimyouknow 아래 테스트 케이스에 맞게 formatSingleDigit 수정하기
-    it.todo('🟢 정수가 아닌 숫자는 그대로 반환한다.', () => {
+    it('🟢 정수가 아닌 숫자는 그대로 반환한다.', () => {
       expect(formatSingleDigit(5.5)).toBe('5.5');
       expect(formatSingleDigit(9.9)).toBe('9.9');
     });
