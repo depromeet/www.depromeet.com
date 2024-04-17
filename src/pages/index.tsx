47e46e852199225b693717c4b5fb2fb30fd61202
hjy0951 <hjy0951@gmail.com>
2024-04-18 02:24:21 +0900

    feat: 메인 페이지 엔딩 컴포넌트 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,9 @@
-import { FAQ } from '~/components/FAQ';
+import { Ending } from '~/components/Ending';
 import { Intro } from '~/components/Intro';
-import { RecruitTextSection, SignImageSection } from '~/components/Main';
 import { Journey } from '~/components/Main/Journey';
 import { RecruitEntrance } from '~/components/Main/RecruitEntrance';
 import { ProjectCarousel } from '~/components/ProjectCarousel';
-import { ScheduleSection } from '~/components/ScheduleSection';
-import { SectionTitle } from '~/components/SectionTitle';
 import { SEO } from '~/components/SEO';
-import { MEMBER_SCHEDULE, SESSION_SCHEDULES } from '~/constant/schedule';
 
 export default function Root() {
   return (
@@ -19,20 +15,7 @@ export default function Root() {
           <Journey />
           <RecruitEntrance />
           <ProjectCarousel />
-          <section>
-            <SectionTitle
-              label="14th Schedule"
-              title={'14기 일정'}
-              description="세션은 매주 진행되며, 오프라인 세션은 오프라인에서 이뤄집니다."
-            />
-            <ScheduleSection {...MEMBER_SCHEDULE} />
-            <ScheduleSection {...SESSION_SCHEDULES} />
-          </section>
-          <section>
-            <FAQ />
-          </section>
-          <SignImageSection />
-          <RecruitTextSection />
+          <Ending />
         </div>
       </main>
     </>
