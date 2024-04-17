71a9409ff691dc6a3c0aa44e871e69eb9745143c
hjy0951 <hjy0951@gmail.com>
2024-04-17 23:53:58 +0900

    refactor: 공통으로 사용할 링크 버튼 컴포넌트 분리

---
+++ src/components/Main/LinkButton.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { css } from '@emotion/react';
+
+import { theme } from '~/styles/theme';
+
+export function LinkButton({ color, text }: { color: string; text: string }) {
+  return (
+    <Link css={linkCss({ color })} href="/about">
+      {text}
+      <span css={arrowImgContainerCss({ color: color === 'black' ? 'white' : color })}>
+        <Image src="/images/main/arrow.svg" alt={`${text} 버튼`} width={15} height={15} />
+      </span>
+    </Link>
+  );
+}
+
+const linkCss = ({ color }: { color: string }) => css`
+  padding: 16px, 40px, 16px, 48px;
+  width: 205px;
+  height: 62px;
+  display: flex;
+  gap: 8px;
+  ${theme.typos.notosans.semibold20};
+  color: white;
+  background-color: ${color};
+  border-radius: 400px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const arrowImgContainerCss = ({ color }: { color: string }) => css`
+  width: 24px;
+  height: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 400px;
+  background-color: ${color};
+`;
