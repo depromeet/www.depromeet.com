edcc58d10b9fee08ba02a46596055f42dd8ab041
hjy0951 <hjy0951@gmail.com>
2024-04-18 00:00:29 +0900

    chore: 불필요한 공통 레이아웃 css 삭제

--- src/styles/layout.ts
+++ src/styles/layout.ts
@@ -1,7 +1,5 @@
 import { css } from '@emotion/react';
 
-import { mediaQuery } from '~/styles/media';
-
 const layout = {
   maxWidth: '1024px',
 } as const;
@@ -10,17 +8,5 @@ export const commonLayoutCss = css`
   width: 100%;
   max-width: ${layout.maxWidth};
   margin: 0 auto;
-  padding: 0 20px;
-
-  ${mediaQuery('pc')} {
-    width: calc(100vw - 60px);
-    margin: 0 30px;
-    padding: 0;
-  }
-
-  ${mediaQuery('mobile')} {
-    width: calc(100vw - 32px);
-    margin: 0 16px;
-    padding: 0;
-  }
+  padding: 0;
 `;
