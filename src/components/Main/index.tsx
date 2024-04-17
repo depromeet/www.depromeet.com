7d48b5ad1df7fc052a6a26f91643ad934e234720
hjy0951 <hjy0951@gmail.com>
2024-04-18 02:49:08 +0900

    refactor: 메인 페이지 컴포넌트를 한 곳에 모아 barrel export 적용

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,2 +1,7 @@
+export { Ending } from './Ending';
+export { Intro } from './Intro';
+export { Journey } from './Journey';
+export { RecruitEntrance } from './RecruitEntrance';
 export { RecruitTextSection } from './RecruitTextSection';
 export { SignImageSection } from './SignImageSection';
+export { Subscribe } from './Subscribe';
