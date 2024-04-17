758c690bab64c92e7bd4ed9729c52c571251934f
hjy0951 <hjy0951@gmail.com>
2024-04-18 02:24:21 +0900

    feat: 메인 페이지 엔딩 컴포넌트 구현

---
+++ src/components/Ending/Ending.tsx
@@ -0,0 +1,66 @@
+import Image from 'next/image';
+import { css, Theme } from '@emotion/react';
+
+import { mediaQuery } from '~/styles/media';
+
+export function Ending() {
+  return (
+    <section css={containerCss}>
+      <div css={bgImageCss}>
+        <Image
+          src="/images/main/ending-img.svg"
+          alt="15기 모집 엔딩 이미지"
+          width={1024}
+          height={700}
+        />
+      </div>
+      <h1>
+        디프만은 여러분을
+        <br />
+        기다리고 있습니다.
+      </h1>
+    </section>
+  );
+}
+
+const containerCss = (theme: Theme) => css`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${theme.colors.blue};
+
+  h1 {
+    position: absolute;
+    bottom: 10px;
+    left: calc(50% - 118px);
+    ${theme.typos.notosans.semibold32}
+  }
+
+  ${mediaQuery('tablet')} {
+    h1 {
+      left: calc(50% - 85px);
+      ${theme.typos.notosans.semibold24}
+    }
+  }
+
+  ${mediaQuery('mobile')} {
+    h1 {
+      left: calc(50% - 76px);
+      ${theme.typos.notosans.semibold20}
+    }
+  }
+`;
+
+const bgImageCss = css`
+  img {
+    vertical-align: bottom;
+    width: 1024px;
+    height: 700px;
+
+    @media screen and (max-width: 1024px) {
+      width: 100%;
+      height: auto;
+    }
+  }
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,9 @@
-import { FAQ } from '~/components/FAQ';
+import { Ending } from '~/components/Ending';
 import { Intro } from '~/components/Intro';
-import { RecruitTextSection, SignImageSection } from '~/components/Main';
 import { Journey } from '~/components/Main/Journey';
 import { RecruitEntrance } from '~/components/Main/RecruitEntrance';
 import { ProjectCarousel } from '~/components/ProjectCarousel';
-import { ScheduleSection } from '~/components/ScheduleSection';
-import { SectionTitle } from '~/components/SectionTitle';
 import { SEO } from '~/components/SEO';
-import { MEMBER_SCHEDULE, SESSION_SCHEDULES } from '~/constant/schedule';
 
 export default function Root() {
   return (
@@ -19,16 +15,7 @@ export default function Root() {
           <Journey />
           <RecruitEntrance />
           <ProjectCarousel />
-          <section>
-            <SectionTitle label="14th Schedule" title={'14기 일정'} />
-            <ScheduleSection {...MEMBER_SCHEDULE} />
-            <ScheduleSection {...SESSION_SCHEDULES} />
-          </section>
-          <section>
-            <FAQ />
-          </section>
-          <SignImageSection />
-          <RecruitTextSection />
+          <Ending />
         </div>
       </main>
     </>
