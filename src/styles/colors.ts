55e7e68c916695d1efb11e2cd57b6a1739aa9d2a
hjy0951 <hjy0951@gmail.com>
2024-04-17 16:52:38 +0900

    chore: 사용할 폰트, 색상 정의 및 파일 추가

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -12,4 +12,10 @@ export const colors = {
   yellow500: '#E3FF3A',
   yellow400: '#C6E413',
   yellow300: '#A1BB00',
+
+  yellow: '#FFEC86',
+  purple: '#7269CE',
+  blue: '#48A4FF',
+  pink: '#FF94B5',
+  green: '#1EC4B4',
 } as const;
