68a7aed4efac9ed5ab20fd1fb642490abf7a8253
YOOJS1205 <junsang8921@gmail.com>
2024-04-20 20:15:33 +0900

    feat: 후원사 관련 상수 변경

--- src/constant/supports.ts
+++ src/constant/supports.ts
@@ -10,21 +10,21 @@ export type Support = {
 
 export const SUPPORTS: Support[] = [
   {
+    title: '팀스파르타',
+    subTitle: 'SpartaCodingClub',
+    description: '세션 운영을 위한 지원금 100만원과 커뮤니티 참여 혜택을 제공합니다.',
+    img: '/images/support/spartar.png',
+  },
+  {
     title: '네이버클라우드',
     subTitle: 'Navercloud',
-    description: '‘Ncloud’ 최대 100만원 크레딧과<br/>커뮤니티 참여 혜택을 제공합니다.',
+    description: '‘Ncloud’ 최대 100만원 크레딧과 커뮤니티 참여 혜택을 제공합니다.',
     img: '/images/support/naver-cloud.png',
   },
   {
     title: '인프런',
     subTitle: 'Inflearn',
-    description: '인프런 강의 30% 할인쿠폰과 강의 수강을<br/> 위해 총 48만 포인트를 제공합니다.',
+    description: '인프런 강의 30% 할인쿠폰과 강의 수강을 위해 총 48만 포인트를 제공합니다.',
     img: '/images/support/inflearn.png',
-  },
-  {
-    title: '모두의연구소',
-    subTitle: 'Modulabs',
-    description: '오프라인 세션 운영을 위한<br/>공간을 지원합니다.',
-    img: '/images/support/모두의연구소.png',
   },
 ];
