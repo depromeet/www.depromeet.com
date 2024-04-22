8c533d24bc86b6072033225ab9be730d42166c99
hjy0951 <hjy0951@gmail.com>
2024-04-22 21:31:32 +0900

    fix: 환경변수 네이밍 수정(그리팅 -> 나인하이어)

--- src/constant/position.ts
+++ src/constant/position.ts
@@ -17,7 +17,7 @@ export const POSITIONS: Array<ComponentProps<typeof PositionsItem>> = [
   {
     type: 'design',
     title: 'UX/UI',
-    link: process.env.NEXT_PUBLIC_GREETING_DESIGN ?? '',
+    link: process.env.NEXT_PUBLIC_NINEHIRE_DESIGN ?? '',
     description: [
       'Figma를 사용해 프로젝트를 해본 경험이 있으신 분',
       'Figma에서 Component 및 Variable 기능을 능숙하게 활용할 수 있으신 분',
@@ -27,7 +27,7 @@ export const POSITIONS: Array<ComponentProps<typeof PositionsItem>> = [
   {
     type: 'aos',
     title: 'ANDROID',
-    link: process.env.NEXT_PUBLIC_GREETING_AOS ?? '',
+    link: process.env.NEXT_PUBLIC_NINEHIRE_AOS ?? '',
     description: [
       'Retrofit을 이용한 서버 통신(API)을 해본 경험이 있으신 분',
       'Kotlin 개발 경험이 있으신 분',
@@ -37,7 +37,7 @@ export const POSITIONS: Array<ComponentProps<typeof PositionsItem>> = [
   {
     type: 'ios',
     title: 'iOS',
-    link: process.env.NEXT_PUBLIC_GREETING_IOS ?? '',
+    link: process.env.NEXT_PUBLIC_NINEHIRE_IOS ?? '',
     description: [
       'UIKit 혹은 SwiftUI를 통하여 개발을 해본경험이 있으신 분',
       '디자인패턴 (MVC, MVVM)에 대한 기본적인 이해가 있으신 분',
@@ -47,7 +47,7 @@ export const POSITIONS: Array<ComponentProps<typeof PositionsItem>> = [
   {
     type: 'server',
     title: 'SERVER',
-    link: process.env.NEXT_PUBLIC_GREETING_SERVER ?? '',
+    link: process.env.NEXT_PUBLIC_NINEHIRE_SERVER ?? '',
     description: [
       '프로젝트 초기 설계 및 인프라 배포 경험이 있으신 분',
       '하나 이상의 서버 프레임워크를 사용해 주도적으로 참여한 프로젝트가 있으신 분',
@@ -57,7 +57,7 @@ export const POSITIONS: Array<ComponentProps<typeof PositionsItem>> = [
   {
     type: 'web',
     title: 'WEB',
-    link: process.env.NEXT_PUBLIC_GREETING_WEB ?? '',
+    link: process.env.NEXT_PUBLIC_NINEHIRE_WEB ?? '',
     description: [
       '프로젝트 협업에 있어 Git 사용 및 협업 툴 사용에 무리가 없는 분',
       'React, Vue, Angular와 같은 모던 자바스크립트 프레임워크를 사용한 경험이 있으신 분',
