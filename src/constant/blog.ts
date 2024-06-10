f86fbce6fff5cdb06b55be8fad6d8638730f8baa
YOOJS1205 <junsang8921@gmail.com>
2024-06-10 22:47:47 +0900

    feat: 15기 미디엄 컨텐츠 추가

--- src/constant/blog.ts
+++ src/constant/blog.ts
@@ -9,6 +9,39 @@ export interface Blog {
 
 export const BLOG_LIST: Blog[] = [
   {
+    title: '디프만 15기를 시작합니다!',
+    date: '24.06.03',
+    img: '/images/blog/15th_start.png',
+    links: [
+      {
+        type: 'MEDIUM',
+        href: 'https://medium.com/depromeet/%EB%94%94%ED%94%84%EB%A7%8C-15%EA%B8%B0%EB%A5%BC-%EC%8B%9C%EC%9E%91%ED%95%A9%EB%8B%88%EB%8B%A4-5e2d53ddef96',
+      },
+    ],
+  },
+  {
+    title: '디프만은 어떻게 공부할까?<br/>(feat. 인프런)',
+    date: '24.05.31',
+    img: '/images/blog/15th_inflearn.png',
+    links: [
+      {
+        type: 'MEDIUM',
+        href: 'https://medium.com/depromeet/interview-%EB%94%94%ED%94%84%EB%A7%8C%EC%9D%80-%EC%96%B4%EB%96%BB%EA%B2%8C-%EA%B3%B5%EB%B6%80%ED%95%A0%EA%B9%8C-feat-%EC%9D%B8%ED%94%84%EB%9F%B0-9b1c235e9e7f',
+      },
+    ],
+  },
+  {
+    title: '가족 일상 공유 서비스,<br/>삐삐를 개발한 "오잉"팀',
+    date: '24.04.28',
+    img: '/images/blog/bbibbi.png',
+    links: [
+      {
+        type: 'MEDIUM',
+        href: 'https://medium.com/depromeet/interview-%EA%B0%80%EC%A1%B1-%EC%9D%BC%EC%83%81-%EA%B3%B5%EC%9C%A0-%EC%84%9C%EB%B9%84%EC%8A%A4-%EC%82%90%EC%82%90%EB%A5%BC-%EA%B0%9C%EB%B0%9C%ED%95%9C-%EC%98%A4%EC%9E%89%ED%8C%80-694e875973c5',
+      },
+    ],
+  },
+  {
     title: '디프만 14기 최종발표,<br/>성공적인 마무리',
     date: '24.03.03',
     img: '/images/blog/final.png',
