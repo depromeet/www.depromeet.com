0f57eabc9de74b59af397e9b935cb8a0407025e1
klmhyeonwoo <gentlemonster77@likelion.org>
2024-11-12 01:29:01 +0900

    refactor: 알림 신청 클릭 시 새로운 탭으로 뜰 수 있도록 로직 수정 (#380)

--- src/constant/gnb.ts
+++ src/constant/gnb.ts
@@ -2,6 +2,7 @@ export type GNBMenu = {
   name: 'About' | '모집 안내' | '프로젝트' | '블로그' | '지원하기' | '16기 모집 알림 신청';
   href: '/about' | '/recruit' | '/project' | '/blog' | '/apply' | 'https://bit.ly/3YJgDmR';
   type: 'text' | 'button';
+  newTab: boolean;
 };
 
 // TODO: 지원하기 url 넣기
@@ -10,21 +11,25 @@ export const GNB_MENU_NAME: GNBMenu[] = [
     name: 'About',
     href: '/about',
     type: 'text',
+    newTab: false,
   },
   {
     name: '모집 안내',
     href: '/recruit',
     type: 'text',
+    newTab: false,
   },
   {
     name: '프로젝트',
     href: '/project',
     type: 'text',
+    newTab: false,
   },
   {
     name: '블로그',
     href: '/blog',
     type: 'text',
+    newTab: false,
   },
 ];
 
@@ -33,25 +38,30 @@ export const GNB_MOBILE_MENU_NAME: GNBMenu[] = [
     name: 'About',
     href: '/about',
     type: 'text',
+    newTab: false,
   },
   {
     name: '모집 안내',
     href: '/recruit',
     type: 'text',
+    newTab: false,
   },
   {
     name: '프로젝트',
     href: '/project',
     type: 'text',
+    newTab: false,
   },
   {
     name: '블로그',
     href: '/blog',
     type: 'text',
+    newTab: false,
   },
   {
     name: '16기 모집 알림 신청',
     href: 'https://bit.ly/3YJgDmR',
     type: 'text',
+    newTab: true,
   },
 ];
