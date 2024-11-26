3bc0ec2b7fa4a57d93ac0619ca4e41f30af6d0ac
Jiyoung Jung <72294509+Jungjjeong@users.noreply.github.com>
2024-11-27 01:23:02 +0900

    feat: intro 공통 컴포넌트로 교체

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,14 +1,17 @@
 import { SEO } from '~/components/SEO';
 import { BlogContentSection } from '~/features/Blog/BlogContentSection';
-import { BlogIntroSection } from '~/features/Blog/BlogIntroSection';
 import { BlogSubscribeSection } from '~/features/Blog/BlogSubscribeSection';
+import { IntroSection } from '~/features/Common/sections/IntroSection';
 
 export default function blog() {
   return (
     <>
       <SEO title="디프만 - Blog" />
       <main>
-        <BlogIntroSection />
+        <IntroSection
+          defaultImgUrl="/images/16th/blog/blog.svg"
+          mobileImgUrl="/images/16th/blog/blog_m.svg"
+        />
         <BlogContentSection />
         <BlogSubscribeSection />
       </main>
