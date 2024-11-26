ea7be68fa9bdcf14236d2038ccee9a61dbab0138
klmhyeonwoo <gentlemonster77@likelion.org>
2024-11-27 02:14:08 +0900

    feat: fab 버튼 추가

---
+++ public/images/16th/icon/fab/index.ts
@@ -0,0 +1,3 @@
+export { default as ic_chat } from './ic_chat.svg';
+export { default as ic_mail } from './ic_mail.svg'
+export { default as ic_question } from './ic_question.svg'
--- public/images/16th/icon/index.ts
+++ public/images/16th/icon/index.ts
@@ -1,3 +1,4 @@
 export * from './popup';
 export * from './common';
 export * from './blog';
+export * from './fab';
