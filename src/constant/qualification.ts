10cd7790ee1374cd6f9769c5fe20139b88038a65
Jiyoung Jung <72294509+Jungjjeong@users.noreply.github.com>
2024-11-27 00:36:18 +0900

    feat: 줄바꿈 반영

--- src/constant/qualification.ts
+++ src/constant/qualification.ts
@@ -1,26 +1,26 @@
 export const QUALIFICATIONS = [
   {
     index: 1,
-    description: '매주 토요일, 오후 2-5시에 진행되는 정규 세션에 참여할 수 있는 분',
+    description: '매주 토요일, 오후 2-5시에 진행되는 \n정규 세션에 참여할 수 있는 분',
   },
   {
     index: 2,
-    description: '매사에 주도적으로 참여하거나, 문제를 능동적으로 해결한 경험이 있는 분',
+    description: '매사에 주도적으로 참여하거나, \n문제를 능동적으로 해결한 경험이 있는 분',
   },
   {
     index: 3,
-    description: '디자이너와 개발자의 동반 성장에 공감하며, 이를 실천하고자 하는 분',
+    description: '디자이너와 개발자의 동반 성장에 \n공감하며, 이를 실천하고자 하는 분',
   },
   {
     index: 4,
-    description: '무엇인가에 꾸준히 몰입해본 열정이 있는 분',
+    description: '무엇인가에 꾸준히 몰입해본 \n열정이 있는 분',
   },
   {
     index: 5,
-    description: '디프만 기간 동안 맡은 역할을 충분히 이행할 수 있는 책임감 있는 분',
+    description: '디프만 기간 동안 맡은 역할을 충분히 \n이행할 수 있는 책임감 있는 분',
   },
   {
     index: 6,
-    description: '좋은 결과만 추구하는 것이 아닌 모든 과정을 즐기는 태도를 가지신 분',
+    description: '좋은 결과만 추구하는 것이 아닌 \n모든 과정을 즐기는 태도를 가지신 분',
   },
 ];
