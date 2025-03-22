3782719b783cf45727367e1f315b2708fb3ee946
Jiyoung Jung <72294509+Jungjjeong@users.noreply.github.com>
2025-03-22 18:50:49 +0900

    chore: 불필요한 중복 파일 제거 및 년도 반영 수정

--- src/components/TimerContainer/index.tsx
+++
@@ -1,1 +0,0 @@
-export { TimerContainer } from './TimerContainer';
--- src/features/Main/sections/MainSubscribeSection.tsx
+++ src/features/Main/sections/MainSubscribeSection.tsx
@@ -1,4 +1,4 @@
-import { BlogSubscribeSection } from '~/features/Blog/BlogSubscribeSection';
+import { BlogSubscribeSection } from '~/features/Blog/sections/BlogSubscribeSection';
 
 /**
  * * Main 페이지 뉴스레터 구독 section
