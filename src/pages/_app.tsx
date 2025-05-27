70f7a25db303709380484f143321b26661845ddb
fr0gydev <ljh130334@naver.com>
2025-05-28 02:07:39 +0900

    feat: add 17th recruitment notification modal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { domMax, LazyMotion } from 'framer-motion';
 import { FloatingActionButton } from '~/components/FAB';
 import { Footer } from '~/components/Footer';
 import { GNB } from '~/components/GNB';
+import RecruitNotificationModal from '~/components/RecruitNotificationModal';
 import { BASE_URL } from '~/constant/common';
 import { useRecordPageView } from '~/hooks/useRecordPageView';
 import GlobalStyle from '~/styles/globalStyle';
@@ -34,6 +35,7 @@ export default function App({ Component, pageProps }: AppProps & InitialProps) {
         <GlobalStyle />
         <Component {...pageProps} />
         <Footer />
+        <RecruitNotificationModal />
       </LazyMotion>
     </ThemeProvider>
   );
