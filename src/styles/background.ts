a1349c1d9dbf098a3a3aa57ff008446adb4b2ca7
youngju6143 <youngju6143@naver.com>
2025-06-12 00:33:38 +0900

    style: 그리드 선 opacity 수정

--- src/styles/background.ts
+++ src/styles/background.ts
@@ -5,8 +5,8 @@ import { colors } from '~/styles/colors';
 export const sectionBg = css`
   background-image: linear-gradient(
     to right,
-    ${colors.primary.blue}9A 1%,
-    ${colors.primary.gray} 1%
+    ${colors.primary.blue}6A 1.09px,
+    ${colors.primary.gray} 1.09px
   );
   background-size: 60.8px 100%;
   background-position: -12.5px 0;
@@ -14,8 +14,8 @@ export const sectionBg = css`
 export const sectionGridBg = css`
   background-color: ${colors.primary.gray}; /* 배경 회색 먼저 깔기 */
 
-  background-image: linear-gradient(to right, ${colors.primary.blue}9A 1%, transparent 1%),
-    linear-gradient(to bottom, ${colors.primary.blue}9A 1%, transparent 1%);
+  background-image: linear-gradient(to right, ${colors.primary.blue}4A 1.09px, transparent 1.09px),
+    linear-gradient(to bottom, ${colors.primary.blue}4A 1.09px, transparent 1.09px);
   background-size: 60.8px 60.8px;
   background-position: -12.5px 0, 0 -12.5px;
 `;
