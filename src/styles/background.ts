10914e51b25586941c388c13352e254db09e7bf5
youngju6143 <youngju6143@naver.com>
2025-06-13 14:18:56 +0900

    fix: 배경지 중앙 정렬 수정

--- src/styles/background.ts
+++ src/styles/background.ts
@@ -9,7 +9,7 @@ export const sectionBg = css`
     ${colors.primary.gray} 1.09px
   );
   background-size: 60.8px 100%;
-  background-position: -12.5px 0;
+  background-position: 50% 0;
 `;
 export const sectionGridBg = css`
   background-color: ${colors.primary.gray}; /* 배경 회색 먼저 깔기 */
@@ -17,5 +17,5 @@ export const sectionGridBg = css`
   background-image: linear-gradient(to right, ${colors.primary.blue}4A 1.09px, transparent 1.09px),
     linear-gradient(to bottom, ${colors.primary.blue}4A 1.09px, transparent 1.09px);
   background-size: 60.8px 60.8px;
-  background-position: -12.5px 0, 0 -12.5px;
+  background-position: 50% 0;
 `;
