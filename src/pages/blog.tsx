5de47c49978a9b503d9370df9cd7e8944d754684
fr0gydev <ljh130334@naver.com>
2025-06-15 17:54:56 +0900

    feat: 하단 줄자 추가 및 블로그 섹션 완성

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,16 +1,11 @@
 import { SEO } from '~/components/SEO';
 import { BlogContentSection } from '~/features/Blog/sections/BlogContentSection';
-import { IntroSection } from '~/features/Common/sections/IntroSection';
 
 export default function blog() {
   return (
     <>
       <SEO title="디프만 - Blog" />
       <main>
-        <IntroSection
-          defaultImgUrl="/images/16th/blog/blog.svg"
-          mobileImgUrl="/images/16th/blog/blog_m.svg"
-        />
         <BlogContentSection />
         {/* <BlogSubscribeSection /> */}
       </main>
