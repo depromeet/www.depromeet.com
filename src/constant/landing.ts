e081c4a4bf501ebbefff192ac7507734bcf1924e
Jeongki <qq8721443@naver.com>
2025-06-15 19:15:29 +0900

    feat: 랜딩 페이지 인트로 섹션 구현

---
+++ src/constant/landing.ts
@@ -0,0 +1,35 @@
+export const LANDING_PART_POSITIONS = [
+  {
+    name: 'android',
+    distance: 400,
+    angle: 15,
+  },
+  {
+    name: 'ios',
+    distance: 300,
+    angle: -10,
+  },
+  {
+    name: 'web',
+    distance: 300,
+    angle: 150,
+  },
+  {
+    name: 'server',
+    distance: 350,
+    angle: 200,
+  },
+  {
+    name: 'design',
+    distance: 400,
+    angle: 180,
+  },
+];
+
+export const ARROW_URLS = [
+  '/images/17th/arrow-design.png',
+  '/images/17th/arrow-web.png',
+  '/images/17th/arrow-server.png',
+  '/images/17th/arrow-ios.png',
+  '/images/17th/arrow-android.png',
+];
