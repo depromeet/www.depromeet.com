e0f4b00c8e2ae34cc89ab42db805f06122850113
youngju6143 <youngju6143@naver.com>
2025-06-17 14:20:51 +0900

    feat: section 02, 배경지, 헤더 반응형 제작(태블릿, 모바일)

--- src/styles/background.ts
+++ src/styles/background.ts
@@ -2,6 +2,8 @@ import { css } from '@emotion/react';
 
 import { colors } from '~/styles/colors';
 
+import { mediaQuery } from './media';
+
 export const sectionBg = css`
   background-image: linear-gradient(
     to right,
@@ -10,8 +12,11 @@ export const sectionBg = css`
   );
   background-position: calc(50% - 17.7px) 0;
   background-size: 47.93px 100%;
-  /* background-position: 37.25px 0; */
-  /* background-size: 60.8px 100%; */
+
+  ${mediaQuery('mobile')} {
+    background-position: 50% 0;
+    background-size: 32px 100%;
+  }
 `;
 export const sectionGridBg = css`
   background-color: ${colors.primary.gray};
@@ -20,6 +25,8 @@ export const sectionGridBg = css`
     linear-gradient(to bottom, ${colors.primary.blue}4A 1px, transparent 1px);
   background-position: calc(50% - 17.7px) 0;
   background-size: 47.93px 47.93px;
-  /* background-position: 37.25px 0; */
-  /* background-size: 60.8px 60.8px; */
+
+  ${mediaQuery('mobile')} {
+    background-size: 32px 32px;
+  }
 `;
