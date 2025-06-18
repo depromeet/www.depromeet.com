8931f33a945e2d1621767f77448c7838c04f70f9
Jeongki <qq8721443@naver.com>
2025-06-18 23:52:10 +0900

    feat: section9 반응형 구현

--- src/features/Main/sections/MainRecruitSection.tsx
+++ src/features/Main/sections/MainRecruitSection.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { css } from '@emotion/react';
 
-// import { useCheckWindowSize } from '~/hooks/useCheckWindowSize';
+import { useCheckWindowSize } from '~/hooks/useCheckWindowSize';
 import useIsInProgress from '~/hooks/useIsInProgress';
 import { sectionBg } from '~/styles/background';
 import { colors } from '~/styles/colors';
@@ -16,7 +16,7 @@ import { getPathToRecruit } from '~/utils/utils';
  */
 export const MainRecruitSection = () => {
   const router = useRouter();
-  // const { isTargetSize: isMobileSize } = useCheckWindowSize('mobile');
+  const { isTargetSize: isMobileSize } = useCheckWindowSize('mobile');
   const { progressState } = useIsInProgress();
   const { action, label, isDisabled } = getPathToRecruit(router, progressState);
 
@@ -31,8 +31,8 @@ export const MainRecruitSection = () => {
           {label}
         </button>
         <Image
-          width={606}
-          height={404}
+          width={isMobileSize ? 294 : 606}
+          height={isMobileSize ? 196 : 404}
           src={'/images/17th/objet.png'}
           alt={'17기 디퍼 모집을 위한 오브제'}
         />
@@ -53,7 +53,7 @@ const containerCss = css`
   overflow: hidden;
 
   ${mediaQuery('mobile')} {
-    padding: 80px 0 248px 0;
+    padding: 60px 0;
   }
 `;
 
@@ -65,15 +65,26 @@ const text = {
     text-align: center;
     gap: 8px;
     color: white;
+
+    ${mediaQuery('tablet')} {
+      padding: 0 40px;
+    }
+
+    ${mediaQuery('mobile')} {
+      padding: 0 16px;
+    }
   `,
   subCss: css`
     ${theme.typosV3.pretendard.sub1Semibold};
     color: ${theme.colors.primary.blue};
     line-height: 150%;
 
+    ${mediaQuery('tablet')} {
+      ${theme.typosV3.pretendard.sub2Semibold};
+    }
+
     ${mediaQuery('mobile')} {
-      ${theme.typosV2.pretendard.semibold18};
-      line-height: 150%;
+      ${theme.typosV3.pretendard.sub3Semibold};
     }
   `,
 
@@ -81,8 +92,13 @@ const text = {
     ${theme.typosV3.pretendard.head1};
     color: ${theme.colors.primary.darknavy};
 
+    ${mediaQuery('tablet')} {
+      font-size: 28px;
+      letter-spacing: 0.28px;
+    }
+
     ${mediaQuery('mobile')} {
-      ${theme.typosV2.pretendard.bold28};
+      ${theme.typosV3.pretendard.head6};
     }
   `,
 };
@@ -90,7 +106,7 @@ const text = {
 const buttonCss = () => css`
   ${theme.typosV3.pretendard.sub3Semibold};
   padding: 14px 26px;
-  margin: 40px 0;
+  margin: 48px 0;
   border-radius: 300px;
   color: white;
   background: ${theme.colors.primary.darknavy};
@@ -102,6 +118,10 @@ const buttonCss = () => css`
   &:disabled {
     background: ${colors.grey[300]};
     color: ${colors.grey[500]};
+  }
+
+  ${mediaQuery('tablet')} {
+    margin: 40px 0;
   }
 `;
 
