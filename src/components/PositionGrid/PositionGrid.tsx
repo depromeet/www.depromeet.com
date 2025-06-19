60b3b239b5cc1da9140b96eb7d6bae341e780dfe
정현우 <nolang3748@naver.com>
2025-06-19 21:07:13 +0900

    chore: 직군별 설명 수정

--- src/components/PositionGrid/PositionGrid.tsx
+++ src/components/PositionGrid/PositionGrid.tsx
@@ -14,7 +14,7 @@ export const PositionGrid = () => {
       color: '#FF6B9D', // 핑크
       isActive: true,
       hoverDescription:
-        '직군 설명 어쩌구 저쩌구구우우웅웅 프로덕트 디자이너 설명 어쩌구 저쩌구구우우웅웅프로덕트 디자이너 설명 어쩌구 저쩌구구우우웅웅프로덕트 디자이너 설명 어쩌구 저쩌구구우우웅웅',
+        '프로덕트 디자이너로서 Figma를 이용해 화면을 디자인하고 개발자와 소통하며 프로덕트를 만듭니다. UX 리서치부터 UI 설계까지 사용자 중심의 사고로 제품 전반의 디자인을 주도하며, 논리적인 사고를 바탕으로 팀과 원활하게 협업합니다.',
     },
     {
       id: 'android-developer',
@@ -23,7 +23,7 @@ export const PositionGrid = () => {
       color: '#6B73FF', // 보라
       isActive: true,
       hoverDescription:
-        '안드로이드 개발자로서 모바일 앱 개발의 전반적인 과정을 담당합니다. Kotlin과 Java를 활용하여 사용자 친화적인 앱을 만들고, 최신 안드로이드 기술 스택을 적용합니다.',
+        '안드로이드 개발자로서 Kotlin 언어에 대한 이해를 바탕으로, Jetpack Compose 또는 XML을 활용한 UI를 개발합니다. Retrofit 등의 서버 통신 경험과 안드로이드 생태계에 대한 이해를 바탕으로 서비스 흐름을 파악하고, 안정적인 앱 출시를 이끌어냅니다.',
     },
     {
       id: 'ios-developer',
@@ -32,7 +32,7 @@ export const PositionGrid = () => {
       color: '#73B9FF', // 파랑
       isActive: true,
       hoverDescription:
-        'iOS 개발자로서 Swift를 활용하여 아이폰과 아이패드용 앱을 개발합니다. 애플의 디자인 가이드라인을 준수하며 사용자 경험을 최우선으로 하는 앱을 만듭니다.',
+        'iOS 개발자로서 Swift에 대한 이해를 바탕으로, UIKit 또는 SwiftUI를 활용한 UI를 개발합니다. URLSession, Alamofire, Moya 등을 이용한 API 통신 경험을 갖추고 있으며, 팀과 협업하여 앱스토어에 서비스를 성공적으로 배포합니다.',
     },
     {
       id: 'server-developer',
@@ -41,7 +41,7 @@ export const PositionGrid = () => {
       color: '#9E9E9E', // 회색
       isActive: true,
       hoverDescription:
-        '서버 개발자로서 백엔드 시스템 설계와 API 개발을 담당합니다. 클라우드 인프라 구축과 데이터베이스 설계, 성능 최적화를 통해 안정적인 서비스를 제공합니다.',
+        '서버 개발자로서 프로젝트 초기 설계부터 인프라 구축까지 주도적으로 참여하며, 웹 서버 프레임워크를 활용해 프로덕트를 만듭니다. 프로덕트의 문제를 정의하고, 팀과의 협업을 통해 요구사항을 정리한 뒤, 이를 바탕으로 데이터 구조를 설계하고 구현합니다.',
     },
     {
       id: 'web-developer',
@@ -50,7 +50,7 @@ export const PositionGrid = () => {
       color: '#FFD93D', // 노랑
       isActive: true,
       hoverDescription:
-        '웹 개발자로서 프론트엔드 개발을 담당합니다. React, TypeScript 등 최신 기술을 활용하여 사용자 인터페이스를 구현하고, 반응형 웹사이트를 제작합니다.',
+        '웹 개발자로서 Git과 협업 도구에 익숙하며, HTTP 통신에 대한 기본적인 이해를 갖추고 있습니다. REST API 기반의 프론트엔드 개발 경험을 바탕으로 React, Vue, Angular 등 모던 자바스크립트 프레임워크를 활용해 사용자와 맞닿는 서비스를 구현합니다.',
     },
   ];
 
@@ -81,6 +81,7 @@ const containerStyles = css`
   width: 100%;
   margin: 0 auto;
   padding: 20px;
+  padding-bottom: 120px;
   ${sectionGridBg};
 `;
 
@@ -101,9 +102,10 @@ const recruitmentMessageStyles = css`
   width: 240px;
   height: 302px;
   display: flex;
-  align-items: center;
+  align-items: flex-end;
   font-size: 18px;
   color: ${theme.colors.primary.darknavy};
   line-height: 1.5;
+  font-weight: 500;
   background: transparent;
 `;
